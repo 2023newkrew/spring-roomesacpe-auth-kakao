3e6956db813626ad98f2299ebeb482d2ccb26027
boorownie <probitanima11@gmail.com>
2022-12-10 08:31:49 +0900

    default code

---
+++ src/main/java/nextstep/theme/ThemeService.java
@@ -0,0 +1,32 @@
+package nextstep.theme;
+
+import nextstep.support.NotExistEntityException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ThemeService {
+    private ThemeDao themeDao;
+
+    public ThemeService(ThemeDao themeDao) {
+        this.themeDao = themeDao;
+    }
+
+    public Long create(ThemeRequest themeRequest) {
+        return themeDao.save(themeRequest.toEntity());
+    }
+
+    public List<Theme> findAll() {
+        return themeDao.findAll();
+    }
+
+    public void delete(Long id) {
+        Theme theme = themeDao.findById(id);
+        if (theme == null) {
+            throw new NotExistEntityException();
+        }
+
+        themeDao.delete(id);
+    }
+}
