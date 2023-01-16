4d0d8bebff0944a231f466a68020b8329271a224
MibSang <secured8372@gmail.com>
2023-01-16 15:45:36 +0900

    feat: 토큰을 통한 정보조회 구현

--- src/main/java/nextstep/member/MemberController.java
+++ src/main/java/nextstep/member/MemberController.java
@@ -1,29 +1,39 @@
 package nextstep.member;
 
+import nextstep.auth.AuthorizationExtractor;
+import nextstep.auth.JwtTokenProvider;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 
 @RestController
 @RequestMapping("/members")
 public class MemberController {
-    private MemberService memberService;
+    private final MemberService memberService;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public MemberController(MemberService memberService) {
+    public MemberController(MemberService memberService, JwtTokenProvider jwtTokenProvider) {
         this.memberService = memberService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @PostMapping
-    public ResponseEntity createMember(@RequestBody MemberRequest memberRequest) {
+    public ResponseEntity<Void> createMember(@RequestBody MemberRequest memberRequest) {
         Long id = memberService.create(memberRequest);
         return ResponseEntity.created(URI.create("/members/" + id)).build();
     }
 
     @GetMapping("/me")
-    public ResponseEntity me() {
-        Long id = 1L;
-        Member member = memberService.findById(id);
-        return ResponseEntity.ok(member);
+    public ResponseEntity<Member> me(HttpServletRequest httpServletRequest) {
+        String token = AuthorizationExtractor.extract(httpServletRequest);
+        if (jwtTokenProvider.validateToken(token)) {
+            Member member = memberService.findById(Long.parseLong(jwtTokenProvider.getPrincipal(token)));
+            return ResponseEntity.ok(member);
+        }
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
     }
 }
---
+++ src/test/java/nextstep/member/MemberE2ETest.java
@@ -0,0 +1,74 @@
+package nextstep.member;
+
+import io.restassured.RestAssured;
+import nextstep.auth.TokenRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+
+import static org.hamcrest.Matchers.is;
+
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class MemberE2ETest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @DisplayName("멤버를 생성한다")
+    @Test
+    public void create() {
+        MemberRequest body = new MemberRequest("username", "password", "name", "010-1234-5678");
+        RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(body)
+                .when().post("/members")
+                .then().log().all()
+                .statusCode(HttpStatus.CREATED.value());
+    }
+
+    @DisplayName("내 정보를 조회한다")
+    @Test
+    void me() {
+        String token = createToken();
+
+        RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .when().get("/members/me")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .body("username", is("username"));
+    }
+
+    String createToken() {
+        MemberRequest memberRequest = new MemberRequest("username", "password", "name", "010-1234-5678");
+        TokenRequest tokenRequest = new TokenRequest("username", "password");
+
+        RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(memberRequest)
+                .when().post("/members")
+                .then().log().all()
+                .statusCode(HttpStatus.CREATED.value());
+
+        return RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(tokenRequest)
+                .when().post("/login/token")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract().body().jsonPath().get("accessToken");
+    }
+}
