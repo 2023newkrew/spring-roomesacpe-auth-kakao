b69ee39e1435fea4391b3f9c1f924ef748a00b7f
pyun0825 <pyun0825@naver.com>
2023-01-16 22:51:13 +0900

    refactor: add token validation to argumentResolver

--- src/main/java/nextstep/auth/AuthenticationPrincipalArgumentResolver.java
+++ src/main/java/nextstep/auth/AuthenticationPrincipalArgumentResolver.java
@@ -27,6 +27,9 @@ public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArg
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
         String token = AuthorizationExtractor.extract(request).orElseThrow(AuthenticationException::new);
+        if (!jwtTokenProvider.validateToken(token)) {
+            throw new AuthenticationException();
+        }
         return jwtTokenProvider.getPrincipal(token);
     }
 }
