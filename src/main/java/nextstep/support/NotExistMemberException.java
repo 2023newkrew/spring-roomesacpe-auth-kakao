6c9aec3c782437cc727ba91d52ab82dbd996f398
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-16 17:11:58 +0900

    feat: NotExistMemberException 추가

---
+++ src/main/java/nextstep/support/NotExistMemberException.java
@@ -0,0 +1,21 @@
+package nextstep.support;
+
+public class NotExistMemberException extends NotExistEntityException {
+    public NotExistMemberException() {
+        super();
+    }
+
+    public NotExistMemberException(String message) {
+        super(message);
+    }
+
+
+    public NotExistMemberException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+
+    public NotExistMemberException(Throwable cause) {
+        super(cause);
+    }
+}
