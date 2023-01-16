75278da12d68ef6f00f400f0b5c89cdfb89140c8
kokomo0 <50624525+kokomo0@users.noreply.github.com>
2023-01-16 15:52:15 +0900

    feat: checkPassword 기능 구현

--- src/main/java/nextstep/auth/AuthService.java
+++ src/main/java/nextstep/auth/AuthService.java
@@ -1,19 +1,44 @@
 package nextstep.auth;
 
+import nextstep.member.Member;
+import nextstep.member.MemberDao;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class AuthService {
     private final JwtTokenProvider jwtTokenProvider;
+    private final MemberDao memberDao;
 
-    public AuthService(JwtTokenProvider jwtTokenProvider) {
+    public AuthService(JwtTokenProvider jwtTokenProvider, JdbcTemplate jdbcTemplate) {
         this.jwtTokenProvider = jwtTokenProvider;
+        this.memberDao = new MemberDao(jdbcTemplate);
     }
 
     public TokenResponse createToken(TokenRequest tokenRequest) {
 
+        if (!checkPassword(tokenRequest)) {
+            // TODO: throw exception
+            return null;
+        }
         String accessToken = jwtTokenProvider.createToken(tokenRequest.getUsername());
         return new TokenResponse(accessToken);
     }
 
+    public boolean checkPassword(TokenRequest tokenRequest) {
+        String username = tokenRequest.getUsername();
+        String password = tokenRequest.getPassword();
+        Optional<Member> actualMember = memberDao.findByUsername(username);
+
+        if (Objects.isNull(actualMember)) {
+            return false;
+        }
+        if (actualMember.get().getPassword() != password) {
+            return false;
+        }
+        return true;
+    }
 }
