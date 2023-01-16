c3e02ff2d2719ce0c3d4db7a4a9c1872d1038dd7
soony.kwonn <soony.kwonn@kakaocorp.com>
2023-01-16 16:36:52 +0900

    refactor: java 컨벤션 적용

--- src/main/java/nextstep/auth/JwtTokenProvider.java
+++ src/main/java/nextstep/auth/JwtTokenProvider.java
@@ -1,21 +1,18 @@
 package nextstep.auth;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
-    private String secretKey = "learning-test-spring";
-    private long validityInMilliseconds = 3600000;
+    private final String secretKey = "learning-test-spring";
+    private final Long validityInMilliseconds = 3600000L;
 
     public String createToken(String principal) {
-        Claims claims = Jwts.claims().setSubject(principal);
+        Claims claims = Jwts.claims()
+                .setSubject(principal);
         Date now = new Date();
         Date validity = new Date(now.getTime() + validityInMilliseconds);
 
@@ -28,14 +25,22 @@ public class JwtTokenProvider {
     }
 
     public String getPrincipal(String token) {
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
     }
 
     public boolean validateToken(String token) {
         try {
-            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            Jws<Claims> claims = Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .parseClaimsJws(token);
 
-            return !claims.getBody().getExpiration().before(new Date());
+            return !claims.getBody()
+                    .getExpiration()
+                    .before(new Date());
         } catch (JwtException | IllegalArgumentException e) {
             return false;
         }
