05697cb83bbc7c8ff2f8db5dd61f27e1cf305aa7
kjaehyeon <jacob9404@gmail.com>
2023-01-16 17:07:53 +0900

    feat: 멤버 로그인시 예외처리 구현

--- src/main/java/nextstep/auth/AuthService.java
+++ src/main/java/nextstep/auth/AuthService.java
@@ -1,14 +1,31 @@
 package nextstep.auth;
 
 import lombok.RequiredArgsConstructor;
+import nextstep.member.Member;
+import nextstep.member.MemberDao;
+import nextstep.support.NotExistMemberException;
+import nextstep.support.PasswordNotMatchException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
     private final JwtTokenProvider jwtTokenProvider;
+    private final MemberDao memberDao;
 
     public TokenResponse createToken(TokenRequest tokenRequest) {
-        return TokenResponse.of(jwtTokenProvider.createToken(tokenRequest.getUsername()));
+        Member member = memberDao.findByUsername(tokenRequest.getUsername());
+        if(ObjectUtils.isEmpty(member)) {
+            throw new NotExistMemberException();
+        }
+
+        if(!member.getPassword().equals(tokenRequest.getPassword())){
+            throw new PasswordNotMatchException();
+        }
+
+        return TokenResponse.of(
+                jwtTokenProvider.createToken(tokenRequest.getUsername())
+        );
     }
 }
