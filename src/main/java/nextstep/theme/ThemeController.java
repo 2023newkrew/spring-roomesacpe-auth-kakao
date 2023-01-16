3ff0c99c563602a78feee19da4870b3e3c34a055
joelonsw <joel610naver@gmail.com>
2023-01-16 18:06:43 +0900

    style: 코드 포맷팅

--- src/main/java/nextstep/theme/ThemeController.java
+++ src/main/java/nextstep/theme/ThemeController.java
@@ -1,13 +1,7 @@
 package nextstep.theme;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
@@ -15,7 +9,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/themes")
 public class ThemeController {
-    private ThemeService themeService;
+    private final ThemeService themeService;
 
     public ThemeController(ThemeService themeService) {
         this.themeService = themeService;
