279e44f96e92edfbf50a3c768180ad1462608717
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-16 17:10:55 +0900

    feat: 사용자 정의 예외의 생성자 추가

--- src/main/java/nextstep/support/DuplicateEntityException.java
+++ src/main/java/nextstep/support/DuplicateEntityException.java
@@ -1,4 +1,21 @@
 package nextstep.support;
 
 public class DuplicateEntityException extends RuntimeException {
+    public DuplicateEntityException() {
+        super();
+    }
+
+    public DuplicateEntityException(String message) {
+        super(message);
+    }
+
+
+    public DuplicateEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+
+    public DuplicateEntityException(Throwable cause) {
+        super(cause);
+    }
 }
--- src/main/java/nextstep/support/NotExistEntityException.java
+++ src/main/java/nextstep/support/NotExistEntityException.java
@@ -1,4 +1,21 @@
 package nextstep.support;
 
 public class NotExistEntityException extends RuntimeException {
+    public NotExistEntityException() {
+        super();
+    }
+
+    public NotExistEntityException(String message) {
+        super(message);
+    }
+
+
+    public NotExistEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+
+    public NotExistEntityException(Throwable cause) {
+        super(cause);
+    }
 }
