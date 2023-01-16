c06f6926fe4a28e568ab04caf053669b0562a79b
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-16 17:12:35 +0900

    feat: NotCorrectPasswordException 추가

---
+++ src/main/java/nextstep/support/NotCorrectPasswordException.java
@@ -0,0 +1,21 @@
+package nextstep.support;
+
+public class NotCorrectPasswordException extends RuntimeException {
+    public NotCorrectPasswordException() {
+        super();
+    }
+
+    public NotCorrectPasswordException(String message) {
+        super(message);
+    }
+
+
+    public NotCorrectPasswordException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+
+    public NotCorrectPasswordException(Throwable cause) {
+        super(cause);
+    }
+}
