a450fe3cfb84f6469e8ffaeab54396f4925d4f3b
Siryeong <SiryeongChoi@gmail.com>
2023-01-16 16:30:26 +0900

    feat: 토큰을 통해 내 정보를 조회하는 기능 구현

--- src/main/java/nextstep/member/MemberService.java
+++ src/main/java/nextstep/member/MemberService.java
@@ -1,13 +1,16 @@
 package nextstep.member;
 
+import nextstep.auth.JwtTokenProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
-    private MemberDao memberDao;
+    private final MemberDao memberDao;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public MemberService(MemberDao memberDao) {
+    public MemberService(MemberDao memberDao, JwtTokenProvider jwtTokenProvider) {
         this.memberDao = memberDao;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     public Long create(MemberRequest memberRequest) {
@@ -16,5 +19,10 @@ public class MemberService {
 
     public Member findById(Long id) {
         return memberDao.findById(id);
+    }
+
+    public Long getPrincipal(String authorizationHeader){
+        Long id = Long.parseLong(jwtTokenProvider.getPrincipal(authorizationHeader.split(" ")[1]));
+        return id;
     }
 }
