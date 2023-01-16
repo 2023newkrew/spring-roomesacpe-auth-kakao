4b8053f51f969325ee56f264ce962d354c6ca963
jin.100 <jin.100@kakaocorp.com>
2023-01-16 14:49:38 +0900

    feat : 토큰 발급하는 API 생성

---
+++ src/main/java/nextstep/auth/AuthController.java
@@ -0,0 +1,23 @@
+package nextstep.auth;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login/token")
+    public ResponseEntity<TokenResponse> produceToken(@RequestBody TokenRequest tokenRequest) {
+        return ResponseEntity.ok(authService.createToken(tokenRequest));
+    }
+
+
+}
---
+++ src/main/java/nextstep/auth/AuthService.java
@@ -0,0 +1,39 @@
+package nextstep.auth;
+
+import nextstep.member.Member;
+import nextstep.member.MemberDao;
+import nextstep.support.AuthorizationException;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class AuthService {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final MemberDao memberDao;
+
+    public AuthService(JwtTokenProvider jwtTokenProvider, MemberDao memberDao) {
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.memberDao = memberDao;
+    }
+
+    public TokenResponse createToken(TokenRequest tokenRequest) {
+        validateUsernamePassword(tokenRequest.getUsername(), tokenRequest.getPassword());
+
+        String accessToken = jwtTokenProvider.createToken(tokenRequest.getUsername());
+        return new TokenResponse(accessToken);
+    }
+
+    private void validateUsernamePassword(String username, String password) throws AuthorizationException {
+        Member member = memberDao.findByUsername(username);
+
+        if (member == null) {
+            throw new AuthorizationException();
+        }
+
+        if (!Objects.equals(member.getPassword(), password)) {
+            throw new AuthorizationException();
+        }
+    }
+}
