c63067941f7c76425b6d3d31beb515d0d842dea6
kokomo0 <50624525+kokomo0@users.noreply.github.com>
2023-01-16 18:57:06 +0900

    fix: /memebers/me 수정

--- src/main/java/nextstep/member/MemberController.java
+++ src/main/java/nextstep/member/MemberController.java
@@ -1,8 +1,11 @@
 package nextstep.member;
 
+import nextstep.auth.AuthorizationExtractor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 import java.util.Optional;
 
@@ -22,9 +25,13 @@ public class MemberController {
     }
 
     @GetMapping("/me")
-    public ResponseEntity me() {
-        Long id = 1L;
-        Optional<Member> member = memberService.findById(id);
-        return ResponseEntity.ok(member.get());
+    public ResponseEntity me(HttpServletRequest request) {
+        String token = AuthorizationExtractor.extract(request);
+        Optional<Member> member = memberService.findMemberByToken(token);
+        if (member.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("토큰 안됨");
+        }
+        return ResponseEntity.ok().body(member.get());
     }
+
 }
