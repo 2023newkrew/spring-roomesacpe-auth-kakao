109be0e7a6889bd38eba4a89f34da8185cf0091b
Neurumaru <ksj6659@gmail.com>
2023-01-16 18:14:53 +0900

    fix: Exception 변경

--- src/main/java/nextstep/support/NotExistEntityException.java
+++ src/main/java/nextstep/support/NotExistEntityException.java
@@ -1,10 +1,5 @@
 package nextstep.support;
 
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.UNAUTHORIZED, reason = "NotExistEntityException")
 public class NotExistEntityException extends RuntimeException {
 
 }
