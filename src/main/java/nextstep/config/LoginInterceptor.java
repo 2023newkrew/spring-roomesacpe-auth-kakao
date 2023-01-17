5e0ad9a17aa84a9ae9925b47bb573e61f2a71774
Sangyups <feels4637@gmail.com>
2023-01-17 17:23:32 +0900

    refactor: 주석 처리한 코드 삭제

--- src/main/java/nextstep/config/LoginInterceptor.java
+++ src/main/java/nextstep/config/LoginInterceptor.java
@@ -1,23 +1,20 @@
 package nextstep.config;
 
-import nextstep.support.AuthorizationException;
-import nextstep.support.DuplicateEntityException;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import nextstep.support.AuthorizationException;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
 public class LoginInterceptor extends HandlerInterceptorAdapter {
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
         String accessToken = request.getHeader("Authorization");
-        if(!HttpMethod.GET.matches(request.getMethod()) && accessToken == null) {
-//            response.sendError(401);
+        if (!HttpMethod.GET.matches(request.getMethod()) && accessToken == null) {
             throw new AuthorizationException();
         }
-
 
         return super.preHandle(request, response, handler);
     }
