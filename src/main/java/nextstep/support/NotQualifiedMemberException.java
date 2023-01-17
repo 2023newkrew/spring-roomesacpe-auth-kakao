f8edab834b9fc2d9b7ea6c0c8a4b10d5ba59dc73
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-17 15:35:51 +0900

    feat: NotQualifiedMemberException 추가

---
+++ src/main/java/nextstep/support/NotQualifiedMemberException.java
@@ -0,0 +1,21 @@
+package nextstep.support;
+
+public class NotQualifiedMemberException extends RuntimeException {
+    public NotQualifiedMemberException() {
+        super();
+    }
+
+    public NotQualifiedMemberException(String message) {
+        super(message);
+    }
+
+
+    public NotQualifiedMemberException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+
+    public NotQualifiedMemberException(Throwable cause) {
+        super(cause);
+    }
+}
