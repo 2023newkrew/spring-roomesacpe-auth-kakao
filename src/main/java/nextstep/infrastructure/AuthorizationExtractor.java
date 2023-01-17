fb14692aa3c85765d0507d1af9d85f6a4b16ceb5
soony.kwonn <soony.kwonn@kakaocorp.com>
2023-01-17 17:19:17 +0900

    refactor: lombok, java 컨벤션 적용

--- src/main/java/nextstep/infrastructure/AuthorizationExtractor.java
+++ src/main/java/nextstep/infrastructure/AuthorizationExtractor.java
@@ -12,14 +12,17 @@ public class AuthorizationExtractor {
 
     public static String extract(HttpServletRequest request) {
         Enumeration<String> headers = request.getHeaders(AUTHORIZATION);
-        if(!headers.hasMoreElements()){
+        if (!headers.hasMoreElements()) {
             throw new NoAccessTokenException("액세스 토큰이 존재하지 않습니다.");
         }
         while (headers.hasMoreElements()) {
             String value = headers.nextElement();
-            if ((value.toLowerCase().startsWith(BEARER_TYPE.toLowerCase()))) {
-                String authHeaderValue = value.substring(BEARER_TYPE.length()).trim();
-                request.setAttribute(ACCESS_TOKEN_TYPE, value.substring(0, BEARER_TYPE.length()).trim());
+            if ((value.toLowerCase()
+                    .startsWith(BEARER_TYPE.toLowerCase()))) {
+                String authHeaderValue = value.substring(BEARER_TYPE.length())
+                        .trim();
+                request.setAttribute(ACCESS_TOKEN_TYPE, value.substring(0, BEARER_TYPE.length())
+                        .trim());
                 int commaIndex = authHeaderValue.indexOf(',');
                 if (commaIndex > 0) {
                     authHeaderValue = authHeaderValue.substring(0, commaIndex);
--- src/main/java/nextstep/reservation/ReservationRequest.java
+++ src/main/java/nextstep/reservation/ReservationRequest.java
@@ -1,22 +1,13 @@
 package nextstep.reservation;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReservationRequest {
     private Long scheduleId;
-    private String name;
 
-    public ReservationRequest() {
-    }
-
-    public ReservationRequest(Long scheduleId, String name) {
-        this.scheduleId = scheduleId;
-        this.name = name;
-    }
-
-    public Long getScheduleId() {
-        return scheduleId;
-    }
-
-    public String getName() {
-        return name;
-    }
 }
