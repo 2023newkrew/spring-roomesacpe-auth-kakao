c136d874a68016744e4a224364f6744a80ff6346
Neurumaru <ksj6659@gmail.com>
2023-01-17 15:22:59 +0900

    refactor: LoginE2ETest 다른 테스트와 비슷한 방식으로 변경

--- src/test/java/nextstep/auth/LoginE2ETest.java
+++ src/test/java/nextstep/auth/LoginE2ETest.java
@@ -1,28 +1,36 @@
 package nextstep.auth;
 
 import io.restassured.RestAssured;
+import nextstep.member.Member;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.jdbc.Sql;
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@Sql("classpath:/test.sql")
 public class LoginE2ETest {
 
     @DisplayName("토큰을 생성한다")
     @Test
     public void createToken() {
-        TokenRequest body = new TokenRequest("user", "pw");
+        Member member = new Member("user", "pw", "user", "010-0000-0000");
+        TokenRequest tokenRequest = new TokenRequest("user", "pw");
 
         RestAssured
                 .given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(body)
+                .body(member)
+                .when().post("/members")
+                .then().log().all()
+                .statusCode(HttpStatus.CREATED.value());
+
+        RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(tokenRequest)
                 .when().post("/login/token")
                 .then().log().all()
                 .statusCode(HttpStatus.OK.value());
