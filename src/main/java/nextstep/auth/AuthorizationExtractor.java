ea845f64103674e69dc5fd9b7ecf1a717b97cb65
MibSang <secured8372@gmail.com>
2023-01-17 18:51:15 +0900

    refactor: token 추출시 RequestContextHolder에서 request 받아오도록 수정

--- src/main/java/nextstep/auth/AuthorizationExtractor.java
+++ src/main/java/nextstep/auth/AuthorizationExtractor.java
@@ -1,12 +1,19 @@
 package nextstep.auth;
 
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
 import javax.servlet.http.HttpServletRequest;
 
-public class AuthorizationExtractor {
-    public static final String AUTHORIZATION = "Authorization";
-    public static String BEARER_TYPE = "Bearer";
+import java.util.Objects;
 
-    public static String extract(HttpServletRequest request) {
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+public class AuthorizationExtractor {
+    public static final String BEARER_TYPE = "Bearer";
+
+    public static String extractTokenFromRequest() {
+        HttpServletRequest request = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
         String header = request.getHeader(AUTHORIZATION);
         if (header != null && header.toLowerCase().startsWith(BEARER_TYPE.toLowerCase())) {
             return header.substring(BEARER_TYPE.length()).trim();
