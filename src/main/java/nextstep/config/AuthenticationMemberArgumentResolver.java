39ade7e1cfc2d80c38a79d68975ca6e71da9f700
Sangyups <feels4637@gmail.com>
2023-01-17 17:21:25 +0900

    refactor: spring bean으로 등록하여 di 활용

--- src/main/java/nextstep/config/AuthenticationMemberArgumentResolver.java
+++ src/main/java/nextstep/config/AuthenticationMemberArgumentResolver.java
@@ -2,17 +2,21 @@ package nextstep.config;
 
 import static nextstep.auth.AuthorizationExtractor.getTokenFromHeader;
 
+import lombok.RequiredArgsConstructor;
 import nextstep.auth.JwtTokenProvider;
 import nextstep.support.AuthorizationException;
 import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@Component
+@RequiredArgsConstructor
 public class AuthenticationMemberArgumentResolver implements HandlerMethodArgumentResolver {
 
-    private final JwtTokenProvider jwtTokenProvider = new JwtTokenProvider();
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
--- src/main/java/nextstep/config/WebMvcConfiguration.java
+++ src/main/java/nextstep/config/WebMvcConfiguration.java
@@ -1,19 +1,17 @@
 package nextstep.config;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
-
-
 @Configuration
+@RequiredArgsConstructor
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
+    private final AuthenticationMemberArgumentResolver authenticationMemberArgumentResolver;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -23,6 +21,6 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new AuthenticationMemberArgumentResolver());
+        resolvers.add(authenticationMemberArgumentResolver);
     }
 }
