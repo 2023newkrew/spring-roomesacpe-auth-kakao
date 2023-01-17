05254c3fe20f9aa62261a33876d582368f3fd7d0
baker.gus <baker.gus@kakaocorp.com>
2023-01-17 16:36:58 +0900

    feat: 내 정보 확인 기능 추가
    # Conflicts:
    #	src/main/resources/application.properties

---
+++ src/main/java/nextstep/support/SwaggerConfig.java
@@ -0,0 +1,64 @@
+package nextstep.support;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .securityContexts(List.of(securityContext()))
+                .securitySchemes(apiKey())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                ;
+    }
+
+    private ApiInfo apiInfo() {
+
+        return new ApiInfoBuilder()
+                .title("MyLittleHomepage API")
+                .description("MyLittleHomepage API Docs")
+                .build();
+    }
+
+    private SecurityContext securityContext() {
+
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("accessToken", authorizationScopes));
+    }
+
+    private List<SecurityScheme> apiKey() {
+
+        List<SecurityScheme> list = new ArrayList<>();
+        list.add(new ApiKey("accessToken", "Bearer +accessToken", "header"));
+        return list;
+    }
+}
