4b22ea7a6fc09351698d052997d7e574fc969053
ekzm8523 <mjw8523@gmail.com>
2023-01-17 23:06:10 +0900

    feat: AuthInterceptor 추가

--- src/main/java/nextstep/auth/AuthArgumentResolver.java
+++ src/main/java/nextstep/auth/AuthArgumentResolver.java
@@ -2,18 +2,17 @@ package nextstep.auth;
 
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.core.MethodParameter;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
-@Component
-public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArgumentResolver {
+//@Component
+public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
 
     private final JwtTokenProvider jwtTokenProvider;
 
-    public AuthenticationPrincipalArgumentResolver(JwtTokenProvider jwtTokenProvider) {
+    public AuthArgumentResolver(JwtTokenProvider jwtTokenProvider) {
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
@@ -24,9 +23,9 @@ public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArg
 
     @Override
     public String resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
-            NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+            NativeWebRequest webRequest, WebDataBinderFactory binderFactory){
         HttpServletRequest nativeRequest = webRequest.getNativeRequest(HttpServletRequest.class);
-        String token = AuthorizationExtractor.extract(nativeRequest);
+        String token = (String) nativeRequest.getAttribute("accessToken");
         return jwtTokenProvider.getPrincipal(token);
 
     }
--- src/main/java/nextstep/config/WebMvcConfig.java
+++ src/main/java/nextstep/config/WebMvcConfig.java
@@ -1,8 +1,12 @@
 package nextstep.config;
 
-import nextstep.auth.AuthenticationPrincipalArgumentResolver;
+import nextstep.auth.AuthArgumentResolver;
+import nextstep.auth.JwtTokenProvider;
+import nextstep.auth.AuthInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
@@ -10,15 +14,32 @@ import java.util.List;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    private final AuthenticationPrincipalArgumentResolver authenticationPrincipalArgumentResolver;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public WebMvcConfig(
-            AuthenticationPrincipalArgumentResolver authenticationPrincipalArgumentResolver) {
-        this.authenticationPrincipalArgumentResolver = authenticationPrincipalArgumentResolver;
+    public WebMvcConfig(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Bean
+    public AuthInterceptor authInterceptor() {
+        return new AuthInterceptor(jwtTokenProvider);
+    }
+
+    @Bean
+    public AuthArgumentResolver authArgumentResolver() {
+        return new AuthArgumentResolver(jwtTokenProvider);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login/token", "/members");
     }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(authenticationPrincipalArgumentResolver);
+        resolvers.add(authArgumentResolver());
     }
 }
