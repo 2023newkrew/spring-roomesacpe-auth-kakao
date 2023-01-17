7f4d6892744b7685f240b512adac9af756c9d43b
kokomo0 <50624525+kokomo0@users.noreply.github.com>
2023-01-17 15:51:36 +0900

    test: 잘못된 토큰 테스트코드 작성

--- src/main/java/nextstep/reservation/ReservationController.java
+++ src/main/java/nextstep/reservation/ReservationController.java
@@ -1,8 +1,10 @@
 package nextstep.reservation;
 
+import nextstep.auth.AuthorizationExtractor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 import java.util.List;
 
@@ -16,9 +18,21 @@ public class ReservationController {
         this.reservationService = reservationService;
     }
 
+
+    /**
+     * public ResponseEntity me(HttpServletRequest request) {
+     *         String token = AuthorizationExtractor.extract(request);
+     *         Optional<Member> member = memberService.findMemberByToken(token);
+     *         if (member.isEmpty()) {
+     *             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("토큰 안됨");
+     *         }
+     *         return ResponseEntity.ok().body(member.get());
+     *     }
+     */
     @PostMapping
-    public ResponseEntity createReservation(@RequestBody ReservationRequest reservationRequest) {
-        Long id = reservationService.create(reservationRequest);
+    public ResponseEntity createReservation(@RequestBody ReservationRequest reservationRequest, HttpServletRequest request) {
+        String token = AuthorizationExtractor.extract(request);
+        Long id = reservationService.create(reservationRequest, token);
         return ResponseEntity.created(URI.create("/reservations/" + id)).build();
     }
 
