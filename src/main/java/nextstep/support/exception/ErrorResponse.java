e43b8f8025ead03e8d4b734699f4dd490c86202b
엄태호 <qqsscc7342@gmail.com>
2023-01-17 19:05:27 +0900

    fix: ExceptionHandler 오류 수정

--- src/main/java/nextstep/support/exception/ErrorResponse.java
+++ src/main/java/nextstep/support/exception/ErrorResponse.java
@@ -1,7 +1,10 @@
 package nextstep.support.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
 @AllArgsConstructor
 public class ErrorResponse {
     private String title;
