f171d3043537288d2b24c1ef03a55741ff02a566
kokomo0 <50624525+kokomo0@users.noreply.github.com>
2023-01-17 18:20:03 +0900

    refact: Argument Resolver를 사용하도록 변경

--- src/main/java/nextstep/member/MemberController.java
+++ src/main/java/nextstep/member/MemberController.java
@@ -1,13 +1,10 @@
 package nextstep.member;
 
-import nextstep.auth.AuthorizationExtractor;
-import org.springframework.http.HttpStatus;
+import nextstep.auth.AuthenticationPrincipal;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/members")
@@ -25,13 +22,8 @@ public class MemberController {
     }
 
     @GetMapping("/me")
-    public ResponseEntity me(HttpServletRequest request) {
-        String token = AuthorizationExtractor.extract(request);
-        Optional<Member> member = memberService.findMemberByToken(token);
-        if (member.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("토큰 안됨");
-        }
-        return ResponseEntity.ok().body(member.get());
+    public ResponseEntity me(@AuthenticationPrincipal Member member) {
+        return ResponseEntity.ok().body(member);
     }
 
 }
