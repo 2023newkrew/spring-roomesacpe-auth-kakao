7e214f33869416cd8cfff2cec806e8dfadb8ef4a
MibSang <secured8372@gmail.com>
2023-01-18 10:32:55 +0900

    feat: 메인 껍데기 페이지 추가

---
+++ src/main/java/nextstep/MainController.java
@@ -0,0 +1,15 @@
+package nextstep;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String mainPage() {
+        return "hello!";
+    }
+}
