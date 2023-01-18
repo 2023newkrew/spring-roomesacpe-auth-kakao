449e31622a8e5412d569d896793c5698550402f6
jerrie.choi <pinkytime96@gmail.com>
2023-01-18 15:04:53 +0900

    feat: 관리자 전용 매핑에 사용될 인터셉터 생성

---
+++ src/main/java/nextstep/ui/interceptor/AdminInterceptor.java
@@ -0,0 +1,30 @@
+package nextstep.ui.interceptor;
+
+import nextstep.auth.JwtTokenProvider;
+import nextstep.member.MemberRole;
+import nextstep.support.ForbiddenException;
+import nextstep.support.TokenExpirationException;
+import nextstep.support.UnauthorizedException;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AdminInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        JwtTokenProvider jwtTokenProvider = new JwtTokenProvider();
+
+        String header = request.getHeader("authorization");
+        if (header == null) throw new UnauthorizedException();
+
+        String token = header.substring("Bearer ".length());
+        if (!jwtTokenProvider.validateToken(token)) throw new TokenExpirationException();
+
+        if (!jwtTokenProvider.getRole(token).equals(MemberRole.ADMIN)) throw new ForbiddenException();
+
+        return super.preHandle(request, response, handler);
+    }
+    
+}
