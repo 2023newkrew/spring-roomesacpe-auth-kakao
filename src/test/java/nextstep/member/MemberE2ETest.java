c665d533e976b15de01db8502e1b08c206831165
eddie.kk <eddie.kk@kakaocorp.com>
2023-01-18 18:09:26 +0900

    test: 사용자 관련 테스트 코드 작성 - 사용자는 테마/스케줄을 생성/삭제할 수 없다.

--- src/test/java/nextstep/member/MemberE2ETest.java
+++ src/test/java/nextstep/member/MemberE2ETest.java
@@ -3,6 +3,8 @@ package nextstep.member;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.dto.request.MemberRequest;
+import nextstep.dto.request.ScheduleRequest;
+import nextstep.dto.request.ThemeRequest;
 import nextstep.dto.request.TokenRequest;
 import nextstep.dto.response.TokenResponse;
 import org.junit.jupiter.api.Test;
@@ -14,6 +16,8 @@ import org.springframework.test.annotation.DirtiesContext;
 
 import static nextstep.common.fixture.MemberProvider.로그인을_한다;
 import static nextstep.common.fixture.MemberProvider.멤버를_생성한다;
+import static nextstep.common.fixture.ScheduleProvider.스케줄을_생성한다;
+import static nextstep.common.fixture.ThemeProvider.테마를_생성한다;
 import static org.assertj.core.api.Assertions.assertThat;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -72,6 +76,81 @@ public class MemberE2ETest {
                 .body("id", notNullValue())
                 .body("username", equalTo(username))
                 .body("password", equalTo(password));
+    }
+
+    @Test
+    void 일반_사용자는_테마를_생성할_수_없다() {
+        // given
+        String username = "username", password = "password";
+        멤버를_생성한다(new MemberRequest(username, password, "name", "010-0000-0000"));
+        TokenResponse userTokenResponse = 로그인을_한다(new TokenRequest(username, password));
+        ThemeRequest themeRequest = new ThemeRequest("테마이름", "테마설명", 20000);
+
+        // when
+        ExtractableResponse<Response> response = 테마를_생성한다(userTokenResponse.getAccessToken(), themeRequest);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    @Test
+    void 일반_사용자는_테마를_삭제할_수_없다() {
+        // given
+        TokenResponse adminTokenResponse = 로그인을_한다(new TokenRequest("admin", "admin"));
+        ExtractableResponse<Response> response = 테마를_생성한다(adminTokenResponse.getAccessToken(), new ThemeRequest("테마이름", "테마설명", 20000));
+
+        String username = "username", password = "password";
+        멤버를_생성한다(new MemberRequest(username, password, "name", "010-0000-0000"));
+        TokenResponse userTokenResponse = 로그인을_한다(new TokenRequest(username, password));
+
+        given()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + userTokenResponse.getAccessToken())
+
+        // when
+        .when()
+                .delete("/admin" + response.header(HttpHeaders.LOCATION))
+
+        // then
+        .then()
+                .statusCode(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    @Test
+    void 일반_사용자는_스케줄을_생성할_수_없다() {
+        // given
+        String username = "username", password = "password";
+        멤버를_생성한다(new MemberRequest(username, password, "name", "010-0000-0000"));
+        TokenResponse userTokenResponse = 로그인을_한다(new TokenRequest(username, password));
+        ScheduleRequest scheduleRequest = new ScheduleRequest(1L, "2023-01-18", "13:00");
+
+        // when
+        ExtractableResponse<Response> response = 스케줄을_생성한다(userTokenResponse.getAccessToken(), scheduleRequest);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    @Test
+    void 일반_사용자는_스케줄을_삭제할_수_없다() {
+        // given
+        TokenResponse adminTokenResponse = 로그인을_한다(new TokenRequest("admin", "admin"));
+        테마를_생성한다(adminTokenResponse.getAccessToken(), new ThemeRequest("테마이름", "테마설명", 20000));
+        ExtractableResponse<Response> response = 스케줄을_생성한다(adminTokenResponse.getAccessToken(), new ScheduleRequest(1L, "2023-01-18", "13:00"));
+
+        String username = "username", password = "password";
+        멤버를_생성한다(new MemberRequest(username, password, "name", "010-0000-0000"));
+        TokenResponse userTokenResponse = 로그인을_한다(new TokenRequest(username, password));
+
+        given()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + userTokenResponse.getAccessToken())
+
+        // when
+        .when()
+                .delete("/admin" + response.header(HttpHeaders.LOCATION))
+
+        // then
+        .then()
+                .statusCode(HttpStatus.UNAUTHORIZED.value());
     }
 
 }
