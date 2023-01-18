0cd372f01f4bd917dc439d4de8092676bc0733cb
pyun0825 <pyun0825@naver.com>
2023-01-18 15:14:51 +0900

    feat: register RoleInterceptor

--- src/main/java/nextstep/config/WebMvcConfiguration.java
+++ src/main/java/nextstep/config/WebMvcConfiguration.java
@@ -1,8 +1,10 @@
 package nextstep.config;
 
 import nextstep.auth.AuthenticationPrincipalArgumentResolver;
+import nextstep.auth.RoleInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -10,9 +12,17 @@ import java.util.List;
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
     private final AuthenticationPrincipalArgumentResolver authenticationPrincipalArgumentResolver;
+    private final RoleInterceptor roleInterceptor;
 
-    public WebMvcConfiguration(AuthenticationPrincipalArgumentResolver authenticationPrincipalArgumentResolver) {
+    public WebMvcConfiguration(AuthenticationPrincipalArgumentResolver authenticationPrincipalArgumentResolver,
+                               RoleInterceptor roleInterceptor) {
         this.authenticationPrincipalArgumentResolver = authenticationPrincipalArgumentResolver;
+        this.roleInterceptor = roleInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(roleInterceptor).addPathPatterns("/admin/**");
     }
 
     @Override
