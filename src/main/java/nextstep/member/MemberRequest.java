ac1ebe16b3656f3cc23376b2c36dcee2e763d918
pyun0825 <pyun0825@naver.com>
2023-01-18 13:56:35 +0900

    refactor: add column role to Member related classes

--- src/main/java/nextstep/member/MemberRequest.java
+++ src/main/java/nextstep/member/MemberRequest.java
@@ -5,14 +5,16 @@ public class MemberRequest {
     private String password;
     private String name;
     private String phone;
+    private Role role;
 
     public MemberRequest() {}
 
-    public MemberRequest(String username, String password, String name, String phone) {
+    public MemberRequest(String username, String password, String name, String phone, Role role) {
         this.username = username;
         this.password = password;
         this.name = name;
         this.phone = phone;
+        this.role = role;
     }
 
     public String getUsername() {
@@ -31,7 +33,11 @@ public class MemberRequest {
         return phone;
     }
 
+    public Role getRole() {
+        return role;
+    }
+
     public Member toEntity() {
-        return new Member(username, password, name, phone);
+        return new Member(username, password, name, phone, role);
     }
 }
