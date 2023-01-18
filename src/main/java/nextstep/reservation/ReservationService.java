9d3e1ef4e76e19870dbecce117468189dbc87831
jerrie.choi <pinkytime96@gmail.com>
2023-01-18 12:15:30 +0900

    refac: 중복 예약 검증 로직 메서드 분리

--- src/main/java/nextstep/reservation/ReservationService.java
+++ src/main/java/nextstep/reservation/ReservationService.java
@@ -9,6 +9,7 @@ import nextstep.theme.ThemeDao;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -23,10 +24,17 @@ public class ReservationService {
         this.scheduleDao = scheduleDao;
     }
 
+    private void validateSchedule(Long scheduleId) {
+        reservationDao.findByScheduleId(scheduleId)
+                .ifPresent(reservation -> {
+                    throw new DuplicateReservationException();
+                });
+    }
+
     public Long create(Long scheduleId, LoginMember loginMember) {
         Schedule schedule = scheduleDao.findById(scheduleId).orElseThrow(ScheduleNotFoundException::new);
-        reservationDao.findByScheduleId(schedule.getId())
-                .ifPresent(s -> {throw new DuplicateReservationException();});
+        validateSchedule(scheduleId);
+
         Reservation newReservation = new Reservation(
                 schedule,
                 loginMember.getUsername()
@@ -39,7 +47,7 @@ public class ReservationService {
         Theme theme = themeDao.findById(themeId).orElseThrow(ThemeNotFoundException::new);
         return reservationDao.findAllByThemeIdAndDate(themeId, date)
                 .stream()
-                .map(reservation -> ReservationResponse.of(reservation))
+                .map(ReservationResponse::of)
                 .collect(Collectors.toList());
     }
 
