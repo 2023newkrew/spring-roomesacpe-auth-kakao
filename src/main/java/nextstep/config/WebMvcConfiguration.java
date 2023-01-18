3032744b04192492e71f0fd3fb2de9c7c0fedb46
jerrie.choi <pinkytime96@gmail.com>
2023-01-18 15:59:34 +0900

    feat: config에 interceptor 추가

--- src/main/java/nextstep/config/WebMvcConfiguration.java
+++ src/main/java/nextstep/config/WebMvcConfiguration.java
@@ -1,8 +1,10 @@
 package nextstep.config;
 
 import nextstep.ui.AuthenticationPrincipalArgumentResolver;
+import nextstep.ui.interceptor.AdminInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -21,4 +23,9 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
         resolvers.add(authenticationPrincipalArgumentResolver);
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AdminInterceptor())
+                .addPathPatterns("/admin/**");
+    }
 }
