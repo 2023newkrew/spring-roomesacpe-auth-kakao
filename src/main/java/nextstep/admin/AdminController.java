8570f71474d2e6f2d3da4f22574f60d32dda1775
jerrie.choi <pinkytime96@gmail.com>
2023-01-18 15:57:39 +0900

    refac: admin controller url 구체화

--- src/main/java/nextstep/admin/AdminController.java
+++ src/main/java/nextstep/admin/AdminController.java
@@ -16,13 +16,13 @@ public class AdminController {
         this.themeService = themeService;
     }
 
-    @PostMapping
+    @PostMapping("/themes")
     public ResponseEntity<Void> createTheme(@RequestBody ThemeRequest themeRequest) {
         Long id = themeService.create(themeRequest);
         return ResponseEntity.created(URI.create("/themes/" + id)).build();
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/themes/{id}")
     public ResponseEntity<Void> deleteTheme(@PathVariable Long id) {
         themeService.delete(id);
         return ResponseEntity.noContent().build();
--- src/test/java/nextstep/admin/AdminE2ETest.java
+++ src/test/java/nextstep/admin/AdminE2ETest.java
@@ -1,6 +1,8 @@
 package nextstep.admin;
 
 import io.restassured.RestAssured;
+import nextstep.auth.JwtTokenProvider;
+import nextstep.member.MemberRole;
 import nextstep.theme.ThemeRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,12 +16,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class AdminE2ETest {
-    @DisplayName("테마를 생성한다")
+
+    public static final String USERNAME = "username";
+    public static final String USER_TOKEN = new JwtTokenProvider().createToken(USERNAME, MemberRole.USER);
+    public static final String ADMIN_TOKEN = new JwtTokenProvider().createToken(USERNAME, MemberRole.ADMIN);
+
+    @DisplayName("관리자가 테마를 생성한다")
     @Test
-    public void create() {
+    public void createThemeByAdmin() {
         ThemeRequest body = new ThemeRequest("테마이름", "테마설명", 22000);
         RestAssured
-                .given().log().all()
+                .given().header("authorization", "Bearer " + ADMIN_TOKEN).log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(body)
                 .when().post("/admin/themes")
@@ -27,13 +34,26 @@ public class AdminE2ETest {
                 .statusCode(HttpStatus.CREATED.value());
     }
 
-    @DisplayName("테마를 삭제한다")
+    @DisplayName("일반 회원이 테마를 생성하지 못한다")
     @Test
-    void delete() {
+    public void createThemeByUser() {
+        ThemeRequest body = new ThemeRequest("테마이름", "테마설명", 22000);
+        RestAssured
+                .given().header("authorization", "Bearer " + USER_TOKEN).log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(body)
+                .when().post("/admin/themes")
+                .then().log().all()
+                .statusCode(HttpStatus.FORBIDDEN.value());
+    }
+
+    @DisplayName("관리자가 테마를 삭제한다")
+    @Test
+    void deleteThemeByAdmin() {
         Long id = createTheme();
 
         var response = RestAssured
-                .given().log().all()
+                .given().header("authorization", "Bearer " + ADMIN_TOKEN).log().all()
                 .when().delete("/admin/themes/" + id)
                 .then().log().all()
                 .extract();
@@ -41,10 +61,22 @@ public class AdminE2ETest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 
+    @DisplayName("일반 회원이 테마를 삭제하지 못한다")
+    @Test
+    void deleteThemeByUser() {
+        Long id = createTheme();
+
+        RestAssured
+                .given().header("authorization", "Bearer " + USER_TOKEN).log().all()
+                .when().delete("/admin/themes/" + id)
+                .then().log().all()
+                .statusCode(HttpStatus.FORBIDDEN.value());
+    }
+
     public Long createTheme() {
         ThemeRequest body = new ThemeRequest("테마이름", "테마설명", 22000);
         String location = RestAssured
-                .given().log().all()
+                .given().header("authorization", "Bearer " + ADMIN_TOKEN).log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(body)
                 .when().post("/admin/themes")
