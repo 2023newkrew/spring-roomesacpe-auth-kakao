835e79630101ee9d494c3f407c66902a94168ebe
pyun0825 <pyun0825@naver.com>
2023-01-18 15:11:01 +0900

    feat: add RoleInterceptor

---
+++ src/main/java/nextstep/auth/RoleInterceptor.java
@@ -0,0 +1,30 @@
+package nextstep.auth;
+
+import nextstep.member.Role;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.security.sasl.AuthenticationException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class RoleInterceptor implements HandlerInterceptor {
+    private final JwtTokenProvider tokenProvider;
+
+    public RoleInterceptor(JwtTokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String token = AuthorizationExtractor.extract(request).orElseThrow(AuthenticationException::new);
+        if (!tokenProvider.validateToken(token)) {
+            throw new AuthenticationException("Token invalid.");
+        }
+        if (!tokenProvider.getRole(token).equals(Role.ADMIN)) {
+            throw new AuthenticationException("Access not allowed.");
+        }
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+}
