7522bdc1cbba55b728b8c3dddbfc0bf079552a27
jerrie.choi <pinkytime96@gmail.com>
2023-01-18 15:04:31 +0900

    refac: 일부 테마 매핑을 어드민 매핑으로 이동

--- src/main/java/nextstep/theme/ThemeController.java
+++ src/main/java/nextstep/theme/ThemeController.java
@@ -15,21 +15,9 @@ public class ThemeController {
         this.themeService = themeService;
     }
 
-    @PostMapping
-    public ResponseEntity<Void> createTheme(@RequestBody ThemeRequest themeRequest) {
-        Long id = themeService.create(themeRequest);
-        return ResponseEntity.created(URI.create("/themes/" + id)).build();
-    }
-
     @GetMapping
     public ResponseEntity<List<ThemeResponse>> showThemes() {
         List<ThemeResponse> results = themeService.findAll();
         return ResponseEntity.ok().body(results);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteTheme(@PathVariable Long id) {
-        themeService.delete(id);
-        return ResponseEntity.noContent().build();
     }
 }
