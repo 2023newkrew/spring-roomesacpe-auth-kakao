0287c199dde2da1792da3059a4826d7187e832a4
jeothen <jeothen@gmail.com>
2023-01-18 16:30:25 +0900

    feat: find member controller/service

--- src/main/java/nextstep/auth/AuthService.java
+++ src/main/java/nextstep/auth/AuthService.java
@@ -1,17 +1,40 @@
 package nextstep.auth;
 
+import nextstep.member.Member;
+import nextstep.member.MemberDao;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class AuthService {
     private final JwtTokenProvider jwtTokenProvider;
+    private final MemberDao memberDao;
 
-    public AuthService(JwtTokenProvider jwtTokenProvider) {
+    public AuthService(JwtTokenProvider jwtTokenProvider, MemberDao memberDao) {
         this.jwtTokenProvider = jwtTokenProvider;
+        this.memberDao = memberDao;
     }
 
     public TokenResponse issueToken(TokenRequest tokenRequest) {
+        validateMember(tokenRequest);
+
         String token = jwtTokenProvider.createToken(tokenRequest.getUsername());
         return new TokenResponse(token);
+    }
+
+    private void validateMember(TokenRequest tokenRequest) {
+        String username = tokenRequest.getUsername();
+        String password = tokenRequest.getPassword();
+
+        Member member = memberDao.findByUsername(username);
+
+        if (member == null) {
+            throw new IllegalArgumentException("멤버를 찾을 수 없습니다");
+        }
+
+        if (!Objects.equals(member.getPassword(), password)) {
+            throw new IllegalArgumentException("비밀번호가 다릅니다.");
+        }
     }
 }
