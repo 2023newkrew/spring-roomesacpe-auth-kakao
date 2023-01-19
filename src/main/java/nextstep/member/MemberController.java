2127510f674a3f517ec472595b29e8f2f3931602
Seongsu Park <86999506+HolywaterPark@users.noreply.github.com>
2023-01-19 10:43:20 +0900

    chore: *를 사용하지 않고 필요한 것만 import하도록 변경

--- src/main/java/nextstep/member/MemberController.java
+++ src/main/java/nextstep/member/MemberController.java
@@ -2,9 +2,13 @@ package nextstep.member;
 
 import nextstep.auth.AuthenticationPrincipal;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/members")
