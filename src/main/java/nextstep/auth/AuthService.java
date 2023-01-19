932298fbbe7a77f9aea904c519cf4a7f637127ae
jin.100 <jin.100@kakaocorp.com>
2023-01-19 13:47:41 +0900

    refactor : 토큰 파싱 책임 ArgumentResolver에서 AuthService::parseTokenFromRequest로 이동

--- src/main/java/nextstep/auth/AuthService.java
+++ src/main/java/nextstep/auth/AuthService.java
@@ -4,11 +4,14 @@ import nextstep.member.Member;
 import nextstep.member.MemberDao;
 import nextstep.support.UnAuthorizedException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.NativeWebRequest;
 
 import java.util.Objects;
 
 @Service
 public class AuthService {
+    public static final String AUTHORIZATION_HEADER_NAME = "Authorization";
+    public static final String BEARER_TYPE = "Bearer";
 
     private final JwtTokenProvider jwtTokenProvider;
     private final MemberDao memberDao;
@@ -18,9 +21,41 @@ public class AuthService {
         this.memberDao = memberDao;
     }
 
-    public LoginMember findByUsername(String username) {
+    public LoginMember parseTokenFromRequest(NativeWebRequest request) {
+        String header = request.getHeader(AUTHORIZATION_HEADER_NAME);
+        validateHeader(header);
+
+        String token = parseTokenFromHeader(header);
+        validateToken(token);
+
+        String username = jwtTokenProvider.getPrincipal(token);
         Member member = memberDao.findByUsername(username);
         return LoginMember.of(member);
+    }
+
+    private void validateHeader(String header) {
+        if (header == null) {
+            throw new UnAuthorizedException();
+        }
+
+        if (!(header.toLowerCase().startsWith(BEARER_TYPE.toLowerCase()))) {
+            throw new UnAuthorizedException();
+        }
+    }
+
+    private String parseTokenFromHeader(String header) {
+        String token = header.substring(BEARER_TYPE.length()).trim();
+        int commaIndex = token.indexOf(',');
+        if (commaIndex > 0) {
+            token = token.substring(0, commaIndex);
+        }
+        return token;
+    }
+
+    private void validateToken(String token) {
+        if(!jwtTokenProvider.validateToken(token)) {
+            throw new UnAuthorizedException();
+        }
     }
 
     public TokenResponse createToken(TokenRequest tokenRequest) {
--- src/main/java/nextstep/config/auth/LoginUserArgumentResolver.java
+++ src/main/java/nextstep/config/auth/LoginUserArgumentResolver.java
@@ -1,8 +1,6 @@
 package nextstep.config.auth;
 
 import nextstep.auth.AuthService;
-import nextstep.auth.JwtTokenProvider;
-import nextstep.support.UnAuthorizedException;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -12,14 +10,9 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 
 @Component
 public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
-    public static final String AUTHORIZATION = "Authorization";
-    public static final String BEARER_TYPE = "Bearer";
-
-    private final JwtTokenProvider jwtTokenProvider;
     private final AuthService authService;
 
-    public LoginUserArgumentResolver(JwtTokenProvider jwtTokenProvider, AuthService authService) {
-        this.jwtTokenProvider = jwtTokenProvider;
+    public LoginUserArgumentResolver(AuthService authService) {
         this.authService = authService;
     }
 
@@ -30,31 +23,7 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
-        String header = webRequest.getHeader(AUTHORIZATION);
-        if (header == null) {
-            throw new UnAuthorizedException();
-        }
-
-        if (!(header.toLowerCase().startsWith(BEARER_TYPE.toLowerCase()))) {
-            throw new UnAuthorizedException();
-        }
-
-        String token = parseTokenFromHeader(header);
-
-        if(!jwtTokenProvider.validateToken(token)) {
-            throw new UnAuthorizedException();
-        }
-
-        String username = jwtTokenProvider.getPrincipal(token);
-        return authService.findByUsername(username);
+        return authService.parseTokenFromRequest(webRequest);
     }
 
-    private String parseTokenFromHeader(String header) {
-        String token = header.substring(BEARER_TYPE.length()).trim();
-        int commaIndex = token.indexOf(',');
-        if (commaIndex > 0) {
-            token = token.substring(0, commaIndex);
-        }
-        return token;
-    }
 }
