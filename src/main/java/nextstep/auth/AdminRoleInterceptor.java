2800f8006a5e3b3ad0c2c639d1ff4b5d5631dfb8
pyun0825 <pyun0825@naver.com>
2023-01-19 13:53:45 +0900

    refactor: make interceptor query member itself

--- src/main/java/nextstep/auth/AdminRoleInterceptor.java
+++ src/main/java/nextstep/auth/AdminRoleInterceptor.java
@@ -1,6 +1,9 @@
 package nextstep.auth;
 
+import nextstep.member.Member;
+import nextstep.member.MemberService;
 import nextstep.member.Role;
+import nextstep.support.NotExistEntityException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -11,9 +14,11 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 public class AdminRoleInterceptor implements HandlerInterceptor {
     private final JwtTokenProvider tokenProvider;
+    private final MemberService memberService;
 
-    public AdminRoleInterceptor(JwtTokenProvider tokenProvider) {
+    public AdminRoleInterceptor(JwtTokenProvider tokenProvider, MemberService memberService) {
         this.tokenProvider = tokenProvider;
+        this.memberService = memberService;
     }
 
     @Override
@@ -22,9 +27,14 @@ public class AdminRoleInterceptor implements HandlerInterceptor {
         if (!tokenProvider.validateToken(token)) {
             throw new AuthenticationException("Token invalid.");
         }
-        if (!tokenProvider.getRole(token).equals(Role.ADMIN)) {
+        Member loggedInMember = memberService.findById(Long.parseLong(tokenProvider.getPrincipal(token)));
+        if (loggedInMember == null) {
+            throw new NotExistEntityException();
+        }
+        if (!loggedInMember.getRole().equals(Role.ADMIN)) {
             throw new AuthenticationException("Access not allowed.");
         }
+        request.setAttribute("loggedInMember", loggedInMember);
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
 }
