d1298eb885f12f2061be833488286b77ea571fae
jeothen <jeothen@gmail.com>
2023-01-19 13:14:26 +0900

    feat: login Interceptor

--- src/main/java/nextstep/config/WebMvcConfiguration.java
+++ src/main/java/nextstep/config/WebMvcConfiguration.java
@@ -1,9 +1,11 @@
 package nextstep.config;
 
+import nextstep.auth.authorization.LoginInterceptor;
 import nextstep.auth.principal.MemberAuthenticationPrincipalArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -17,6 +19,11 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
         this.memberAuthenticationPrincipalArgumentResolver = memberAuthenticationPrincipalArgumentResolver;
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/reservations/**");
+    }
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(memberAuthenticationPrincipalArgumentResolver);
