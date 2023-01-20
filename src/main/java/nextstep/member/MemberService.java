8d8e07d901e2c67ce2e495893b052dee47b04301
queueSub <kyusch0330@gmail.com>
2023-01-20 15:39:27 +0900

    refactor: MemberService의 불필요한 메서드 제거

--- src/main/java/nextstep/member/MemberService.java
+++ src/main/java/nextstep/member/MemberService.java
@@ -1,11 +1,10 @@
 package nextstep.member;
 
-import nextstep.support.excpetion.NotExistMemberException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
-    private MemberDao memberDao;
+    private final MemberDao memberDao;
 
     public MemberService(MemberDao memberDao) {
         this.memberDao = memberDao;
@@ -14,10 +13,4 @@ public class MemberService {
     public Long create(MemberRequest memberRequest) {
         return memberDao.save(memberRequest.toEntity());
     }
-
-    public Member findById(Long id) {
-        return memberDao.findById(id)
-                .orElseThrow(NotExistMemberException::new);
-    }
-
 }
