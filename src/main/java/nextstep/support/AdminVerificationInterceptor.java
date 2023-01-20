cb9bae0420c0e92fe58d72fe466bb24be5548de0
queueSub <kyusch0330@gmail.com>
2023-01-20 14:13:58 +0900

    feat: admin 검증을 위한 인터셉터 구현

---
+++ src/main/java/nextstep/support/AdminVerificationInterceptor.java
@@ -0,0 +1,46 @@
+package nextstep.support;
+
+import nextstep.auth.AuthorizationTokenExtractor;
+import nextstep.auth.JwtTokenProvider;
+import nextstep.member.Member;
+import nextstep.member.MemberDao;
+import nextstep.support.excpetion.InvalidAuthorizationTokenException;
+import nextstep.support.excpetion.NotExistMemberException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class AdminVerificationInterceptor implements HandlerInterceptor {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final MemberDao memberDao;
+
+    public AdminVerificationInterceptor(JwtTokenProvider jwtTokenProvider, MemberDao memberDao) {
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.memberDao = memberDao;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String token = AuthorizationTokenExtractor.extract(request.getHeader(AuthorizationTokenExtractor.AUTHORIZATION))
+                .orElseThrow(InvalidAuthorizationTokenException::new);
+
+        if (!jwtTokenProvider.validateToken(token)) {
+            throw new InvalidAuthorizationTokenException();
+        }
+
+        Member member =  memberDao.findByUsername(jwtTokenProvider.getPrincipal(token))
+                .orElseThrow(NotExistMemberException::new);
+
+        if (!member.hasAdminRole()) {
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            return false;
+        }
+
+        return true;
+    }
+}
