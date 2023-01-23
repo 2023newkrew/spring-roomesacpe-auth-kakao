51656fca2388daa52c6f70666505acadd19fc7f6
Ethan <rlfalsgh96@naver.com>
2023-01-23 20:20:16 +0900

    feat : GlobalExceptionHandler에 403, 401, 500 에러 핸드링 추가

--- src/main/java/nextstep/exception/GlobalExceptionHandler.java
+++ src/main/java/nextstep/exception/GlobalExceptionHandler.java
@@ -1,13 +1,31 @@
 package nextstep.exception;
 
+import nextstep.auth.exception.AuthenticationException;
+import nextstep.auth.exception.AuthorizationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler({NullPointerException.class, DuplicateEntityException.class})
-    public ResponseEntity<Void> onNPEAndDuplicatedEntityException(Exception e) {
+    @ExceptionHandler({DuplicateEntityException.class})
+    public ResponseEntity<Void> onDuplicatedEntityException(DuplicateEntityException e) {
         return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler({AuthenticationException.class})
+    public ResponseEntity<Void> onAuthenticationException(AuthenticationException e) {
+        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler({AuthorizationException.class})
+    public ResponseEntity<Void> onAuthorizationException(AuthorizationException e) {
+        return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<Void> onRuntimeException(RuntimeException e) {
+        return ResponseEntity.internalServerError().build();
     }
 }
