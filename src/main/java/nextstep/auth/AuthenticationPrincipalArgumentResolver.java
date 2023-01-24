fa80c214cdf427f70fb4d775c747941b0f2ddb24
pyun0825 <pyun0825@naver.com>
2023-01-25 00:31:28 +0900

    refactor: add new interceptors and change Resolver to get Member from request attribute

--- src/main/java/nextstep/auth/AuthenticationPrincipalArgumentResolver.java
+++ src/main/java/nextstep/auth/AuthenticationPrincipalArgumentResolver.java
@@ -2,15 +2,12 @@ package nextstep.auth;
 
 import nextstep.member.Member;
 import nextstep.member.MemberService;
-import nextstep.support.NotExistEntityException;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
-
-import javax.security.sasl.AuthenticationException;
 import javax.servlet.http.HttpServletRequest;
 
 @Component
@@ -31,14 +28,7 @@ public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArg
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
-        String token = AuthorizationExtractor.extract(request).orElseThrow(() -> new AuthenticationException("Log in required."));
-        if (!jwtTokenProvider.validateToken(token)) {
-            throw new AuthenticationException("Token invalid.");
-        }
-        Member loggedInMember = memberService.findById(Long.parseLong(jwtTokenProvider.getPrincipal(token)));
-        if (loggedInMember == null) {
-            throw new NotExistEntityException();
-        }
+        Member loggedInMember = (Member) request.getAttribute("loggedInMember");
         return LoggedInMember.from(loggedInMember);
     }
 }
--- src/main/java/nextstep/config/WebMvcConfiguration.java
+++ src/main/java/nextstep/config/WebMvcConfiguration.java
@@ -2,7 +2,9 @@ package nextstep.config;
 
 import nextstep.auth.AuthenticationPrincipalArgumentResolver;
 import nextstep.auth.AdminRoleInterceptor;
+import nextstep.auth.PathMatchInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,16 +15,25 @@ import java.util.List;
 public class WebMvcConfiguration implements WebMvcConfigurer {
     private final AuthenticationPrincipalArgumentResolver authenticationPrincipalArgumentResolver;
     private final AdminRoleInterceptor adminRoleInterceptor;
+    private final PathMatchInterceptor pathMatchInterceptor;
 
     public WebMvcConfiguration(AuthenticationPrincipalArgumentResolver authenticationPrincipalArgumentResolver,
-                               AdminRoleInterceptor adminRoleInterceptor) {
+                               AdminRoleInterceptor adminRoleInterceptor,
+                               PathMatchInterceptor pathMatchInterceptor) {
         this.authenticationPrincipalArgumentResolver = authenticationPrincipalArgumentResolver;
         this.adminRoleInterceptor = adminRoleInterceptor;
+        this.pathMatchInterceptor = pathMatchInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(adminRoleInterceptor).addPathPatterns("/admin/**");
+        registry.addInterceptor(
+                pathMatchInterceptor.includePathPattern("/members/me", HttpMethod.GET)
+                        .includePathPattern("/reservations", HttpMethod.POST)
+                        .includePathPattern("/reservations/{\\d+}", HttpMethod.DELETE)
+                )
+                .addPathPatterns("/members/me", "/reservations", "/reservations/{\\d+}");
     }
 
     @Override
