df12b8fbbdf3ff18610f5ee99ef68a37a29d4d9f
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-24 22:46:45 +0900

    test: 사용자 로그인 실패하는 경우 테스트 코드 추가

--- src/test/java/nextstep/auth/AuthServiceTest.java
+++ src/test/java/nextstep/auth/AuthServiceTest.java
@@ -2,7 +2,11 @@ package nextstep.auth;
 
 import io.restassured.RestAssured;
 import nextstep.auth.util.JwtTokenProvider;
+import nextstep.error.ErrorCode;
+import nextstep.exception.NotCorrectPasswordException;
+import nextstep.exception.NotExistEntityException;
 import nextstep.member.MemberRequest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,6 +17,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -38,7 +43,7 @@ class AuthServiceTest {
                 .statusCode(HttpStatus.CREATED.value());
     }
 
-    @DisplayName("username과 password 일치 확인 후 토큰을 발급해서 반환")
+    @DisplayName("Member의 username과 password 일치 확인 후 토큰을 발급해서 반환")
     @Test
     void create() {
         TokenRequest tokenRequest = new TokenRequest(USERNAME, PASSWORD);
@@ -48,5 +53,25 @@ class AuthServiceTest {
         String username = jwtTokenProvider.getPrincipal(tokenResponse.getAccessToken());
 
         assertThat(username).isEqualTo(USERNAME);
+    }
+
+    @DisplayName("username에 해당하는 Member가 DB에 없는 경우 예외 발생")
+    @Test
+    void create_notFound() {
+        TokenRequest tokenRequest = new TokenRequest("wrongUserName", PASSWORD);
+        assertThatThrownBy(() -> authService.createMemberToken(tokenRequest))
+                .isInstanceOf(NotExistEntityException.class)
+                .extracting("errorCode")
+                .isEqualTo(ErrorCode.MEMBER_NOT_FOUND);
+    }
+
+    @DisplayName("Member의 username과 password가 일치하지 않는 경우 예외 발생")
+    @Test
+    void create_wrongPassword() {
+        TokenRequest tokenRequest = new TokenRequest(USERNAME, "wrongPassword");
+        assertThatThrownBy(() -> authService.createMemberToken(tokenRequest))
+                .isInstanceOf(NotCorrectPasswordException.class)
+                .extracting("errorCode")
+                .isEqualTo(ErrorCode.UNAUTHORIZED);
     }
 }
