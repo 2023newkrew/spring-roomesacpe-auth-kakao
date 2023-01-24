3f4ff30044703fa29938a1fc4018242ae872adb9
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-25 00:30:33 +0900

    feat: AdminAuthInterceptor 등록

--- src/main/java/nextstep/config/WebMvcConfiguration.java
+++ src/main/java/nextstep/config/WebMvcConfiguration.java
@@ -1,8 +1,10 @@
 package nextstep.config;
 
+import nextstep.support.AdminAuthInterceptor;
 import nextstep.support.LoginMemberArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -11,13 +13,21 @@ import java.util.List;
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
     private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+    private final AdminAuthInterceptor adminAuthInterceptor;
 
-    public WebMvcConfiguration(LoginMemberArgumentResolver loginMemberArgumentResolver) {
+    public WebMvcConfiguration(LoginMemberArgumentResolver loginMemberArgumentResolver, AdminAuthInterceptor adminAuthInterceptor) {
         this.loginMemberArgumentResolver = loginMemberArgumentResolver;
+        this.adminAuthInterceptor = adminAuthInterceptor;
     }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginMemberArgumentResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(adminAuthInterceptor)
+                .addPathPatterns("/admin/**");
     }
 }
