e74adf1f9ce9189b3d4817d427bc806fa1babf93
SJGeeKo <sungjun.gee.ko@gmail.com>
2023-01-25 00:07:24 +0900

    feat: token 생성시 username과 password 확인 로직 추가

--- src/main/java/nextstep/auth/AuthService.java
+++ src/main/java/nextstep/auth/AuthService.java
@@ -1,20 +1,34 @@
 package nextstep.auth;
 
+import nextstep.member.Member;
+import nextstep.member.MemberDao;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final MemberDao memberDao;
 
-    public AuthService(JwtTokenProvider jwtTokenProvider) {
+    public AuthService(JwtTokenProvider jwtTokenProvider, MemberDao memberDao) {
         this.jwtTokenProvider = jwtTokenProvider;
+        this.memberDao = memberDao;
     }
 
     public TokenResponse createToken(TokenRequest tokenRequest) {
-        //TODO: validation 로직 추가해야함
+        validateTokenRequest(tokenRequest);
 
         String accessToken = jwtTokenProvider.createToken(tokenRequest.getUsername());
         return new TokenResponse(accessToken);
+    }
+
+    private void validateTokenRequest(TokenRequest tokenRequest) {
+        Member member = memberDao.findByUsername(tokenRequest.getUsername());
+        if (member == null) {
+            throw new NullPointerException("유저가 존재하지 않습니다.");
+        }
+        if (member.checkWrongPassword(tokenRequest.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
     }
 }
