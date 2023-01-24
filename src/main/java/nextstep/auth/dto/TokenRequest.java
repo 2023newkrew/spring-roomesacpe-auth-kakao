09b4f8f4694ea53018e2cf42eb9fd3da72716c8a
BaeRoNuI <xogh20321@gmail.com>
2023-01-24 23:16:09 +0900

    refactor: apply lombok

--- src/main/java/nextstep/auth/dto/TokenRequest.java
+++ src/main/java/nextstep/auth/dto/TokenRequest.java
@@ -1,21 +1,13 @@
 package nextstep.auth.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class TokenRequest {
     private String username;
     private String password;
-
-    public TokenRequest() {}
-
-    public TokenRequest(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 }
