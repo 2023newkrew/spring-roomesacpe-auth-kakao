faa1b1b17ab11e71b9ba659fc03af9694cad57a1
Siryeong <SiryeongChoi@gmail.com>
2023-01-24 15:18:07 +0900

    refactor: 관리자 관련 api는 /admin uri를 붙여 관리한다.

---
+++ src/main/java/nextstep/admin/AdminThemeController.java
@@ -0,0 +1,31 @@
+package nextstep.admin;
+
+import nextstep.theme.ThemeRequest;
+import nextstep.theme.ThemeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/admin/themes")
+public class AdminThemeController {
+    private ThemeService themeService;
+
+    public AdminThemeController(ThemeService themeService) {
+        this.themeService = themeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createTheme(@RequestBody ThemeRequest themeRequest) {
+        Long id = themeService.create(themeRequest);
+        return ResponseEntity.created(URI.create("/themes/" + id)).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTheme(@PathVariable Long id) {
+        themeService.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/nextstep/theme/ThemeController.java
+++ src/main/java/nextstep/theme/ThemeController.java
@@ -21,22 +21,9 @@ public class ThemeController {
         this.themeService = themeService;
     }
 
-    @PostMapping
-    public ResponseEntity<Void> createTheme(@RequestBody ThemeRequest themeRequest) {
-        Long id = themeService.create(themeRequest);
-        return ResponseEntity.created(URI.create("/themes/" + id)).build();
-    }
-
     @GetMapping
     public ResponseEntity<List<Theme>> showThemes() {
         List<Theme> results = themeService.findAll();
         return ResponseEntity.ok().body(results);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteTheme(@PathVariable Long id) {
-        themeService.delete(id);
-
-        return ResponseEntity.noContent().build();
     }
 }
