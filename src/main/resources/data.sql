e84de7479ad32b4199278d66ba8325c056a48164
Siryeong <SiryeongChoi@gmail.com>
2023-01-24 15:31:45 +0900

    chore: 더미유저 이름 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO member (username, password, name, phone, role)
 VALUES
     ('admin', 'admin', 'admin', 'admin', 'ADMIN'),
-    ('username', 'password', 'dummy-user', '010-1234-1234', 'MEMBER');
+    ('username', 'password', 'davi', '010-1234-1234', 'MEMBER');
