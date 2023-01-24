354932ba42d4c401d6ebda2958edaf2ea8e13484
pyun0825 <pyun0825@naver.com>
2023-01-25 00:27:09 +0900

    feat: add LoginInterceptor

---
+++ src/main/java/nextstep/auth/LoginInterceptor.java
@@ -0,0 +1,36 @@
+package nextstep.auth;
+
+import nextstep.member.Member;
+import nextstep.member.MemberService;
+import nextstep.support.NotExistEntityException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.security.sasl.AuthenticationException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class AnyRoleInterceptor implements HandlerInterceptor {
+    private final JwtTokenProvider tokenProvider;
+    private final MemberService memberService;
+
+    public AnyRoleInterceptor(JwtTokenProvider tokenProvider, MemberService memberService) {
+        this.tokenProvider = tokenProvider;
+        this.memberService = memberService;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String token = AuthorizationExtractor.extract(request).orElseThrow(() -> new AuthenticationException("Log in required."));
+        if (!tokenProvider.validateToken(token)) {
+            throw new AuthenticationException("Token invalid.");
+        }
+        Member loggedInMember = memberService.findById(Long.parseLong(tokenProvider.getPrincipal(token)));
+        if (loggedInMember == null) {
+            throw new NotExistEntityException();
+        }
+        request.setAttribute("loggedInMember", loggedInMember);
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+}
