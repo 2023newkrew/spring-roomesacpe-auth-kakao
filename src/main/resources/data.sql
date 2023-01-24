739facd4de96b96ec7bf2b99fadabe0a9868211b
Siryeong <SiryeongChoi@gmail.com>
2023-01-24 15:00:54 +0900

    chore: 일반 유저 더미 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
-insert into member (username, password, name, phone, role)
-values ('admin', 'admin', 'admin', 'admin', 'ADMIN');
+INSERT INTO member (username, password, name, phone, role)
+VALUES
+    ('admin', 'admin', 'admin', 'admin', 'ADMIN'),
+    ('username', 'password', 'dummy-user', '010-1234-1234', 'MEMBER');
