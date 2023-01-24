d21d46daa4984b7a4b796c6063ac45f9cc60e060
BaeRoNuI <xogh20321@gmail.com>
2023-01-25 01:40:48 +0900

    feat: seperate member entity and dtos

---
+++ src/main/java/nextstep/member/mapper/MemberMapper.java
@@ -0,0 +1,32 @@
+package nextstep.member.mapper;
+
+import lombok.NoArgsConstructor;
+import nextstep.global.util.PasswordUtility;
+import nextstep.member.dto.MemberRequest;
+import nextstep.member.dto.MemberResponse;
+import nextstep.member.entity.MemberEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+@NoArgsConstructor
+public class MemberMapper {
+    private PasswordUtility passwordUtility;
+
+    public MemberEntity requestToEntity(MemberRequest memberRequest) {
+        return new MemberEntity(null,
+                memberRequest.getUsername(),
+                passwordUtility.encrypt(memberRequest.getPassword()),
+                memberRequest.getName(),
+                memberRequest.getPhone());
+    }
+
+    public MemberResponse entityToResponse(MemberEntity memberEntity) {
+        return new MemberResponse(
+                memberEntity.getId(),
+                memberEntity.getUsername(),
+                memberEntity.getPassword(),
+                memberEntity.getName(),
+                memberEntity.getPhone()
+        );
+    }
+}
