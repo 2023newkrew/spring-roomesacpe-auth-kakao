0d223a4358268b38495afe03d31b10bef077c071
kimtaehyun98 <kimtaehyun98@naver.com>
2023-01-25 10:49:52 +0900

    feat(auth): 관리자 인증 로직 interceptor 구현

--- src/main/java/nextstep/config/AdminInterceptor.java
+++ src/main/java/nextstep/config/AdminInterceptor.java
@@ -2,41 +2,55 @@ package nextstep.config;
 
 import lombok.RequiredArgsConstructor;
 import nextstep.auth.JwtTokenProvider;
+import nextstep.member.Member;
+import nextstep.member.MemberDao;
 import nextstep.support.AuthorizationException;
-import nextstep.support.DuplicateEntityException;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import static nextstep.support.ErrorMessage.LOGIN_FAIL;
-import static nextstep.support.ErrorMessage.TOKEN_EXPIRATION;
+import static nextstep.support.ErrorMessage.*;
 
 @Component
 @RequiredArgsConstructor
-public class LoginInterceptor extends HandlerInterceptorAdapter {
+public class AdminInterceptor extends HandlerInterceptorAdapter {
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final MemberDao memberDao;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String accessToken = request.getHeader("Authorization");
 
-        // GET 메서드가 아닐때 (GET은 예약 목록 조회에만 사용되고 있기 때문에 로그인 하지 않아도 가능해야 함)
-        if(!HttpMethod.GET.matches(request.getMethod())) {
-            // 로그인 검증 로직
-            checkLogin(accessToken);
-        }
+        // 로그인 검증 로직
+        checkLogin(accessToken);
+
+        // 관리자인지 검증
+        checkAdmin(accessToken);
 
         return super.preHandle(request, response, handler);
+    }
+
+    private void checkAdmin(String accessToken){
+        checkRole(accessToken);
     }
 
     private void checkLogin(String accessToken){
         checkNullToken(accessToken);
         checkTokenExpiration(accessToken);
+    }
+
+    // 해당 유저의 Role이 관리자(= 0)인지 확인
+    private void checkRole(String accessToken){
+        String token = accessToken.split(" ")[1];
+        long id = Long.parseLong(jwtTokenProvider.getPrincipal(token));
+        Member member = memberDao.findById(id);
+        if(member.getRole() != 0){
+            throw new AuthorizationException(ADMIN_FAIL);
+        }
     }
 
     // token 값이 비어있는지 확인
--- src/main/java/nextstep/support/ErrorMessage.java
+++ src/main/java/nextstep/support/ErrorMessage.java
@@ -6,5 +6,6 @@ public class ErrorMessage {
     public static final String RESERVATION_FAIL = "예약자의 이름과 일치하지 않습니다.";
     public static final String TOKEN_EXPIRATION = "JWT 토큰 인증이 만료되었습니다.";
     public static final String RESERVATION_NOT_FOUND = "삭제하려는 예약이 존재하지 않습니다.";
+    public static final String ADMIN_FAIL = "관리자 계정이 아닙니다.";
 
 }
