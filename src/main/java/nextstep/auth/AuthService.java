ce12f277ed60d1a7627c9414bc52858ca7606eac
herbi.sf8 <herbi.sf8@kakaocorp.com>
2023-01-25 15:20:13 +0900

    style: AuthService에 RequiredArgsConstructor 적용, 기존 생성자 삭제

--- src/main/java/nextstep/auth/AuthService.java
+++ src/main/java/nextstep/auth/AuthService.java
@@ -1,5 +1,6 @@
 package nextstep.auth;
 
+import lombok.RequiredArgsConstructor;
 import nextstep.member.Member;
 import org.springframework.stereotype.Service;
 
@@ -8,12 +9,9 @@ import java.util.Map;
 import static nextstep.auth.JwtTokenConfig.TOKEN_ROLE_KEY;
 
 @Service
+@RequiredArgsConstructor
 public class AuthService {
     private final JwtTokenProvider jwtTokenProvider;
-
-    public AuthService(JwtTokenProvider jwtTokenProvider) {
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
 
     public TokenResponse createToken(Member member) {
         String token = jwtTokenProvider.createToken(member.getUsername(), Map.of(TOKEN_ROLE_KEY, member.getRole().name()));
