d40a06fb4f08c3ed4a5abaaa4112776a20bdfc3b
jayden.v <jayden.v@kakaocorp.com>
2023-01-25 19:11:56 +0900

    feat: 멤버 권한 추가

--- src/main/java/nextstep/member/Member.java
+++ src/main/java/nextstep/member/Member.java
@@ -2,24 +2,23 @@ package nextstep.member;
 
 public class Member {
     private Long id;
-    private String username;
-    private String password;
-    private String name;
-    private String phone;
+    private final String username;
+    private final String password;
+    private final String name;
+    private final String phone;
+    private final Role role;
 
-    public Member(Long id, String username, String password, String name, String phone) {
+    public Member(Long id, String username, String password, String name, String phone, Role role) {
+        this(username, password, name, phone, role);
         this.id = id;
-        this.username = username;
-        this.password = password;
-        this.name = name;
-        this.phone = phone;
     }
 
-    public Member(String username, String password, String name, String phone) {
+    public Member(String username, String password, String name, String phone, Role role) {
         this.username = username;
         this.password = password;
         this.name = name;
         this.phone = phone;
+        this.role = role;
     }
 
     public Long getId() {
@@ -42,7 +41,15 @@ public class Member {
         return phone;
     }
 
+    public Role getRole() {
+        return role;
+    }
+
     public boolean checkWrongPassword(String password) {
         return !this.password.equals(password);
+    }
+
+    public boolean isAdmin() {
+        return this.role == Role.ADMIN;
     }
 }
