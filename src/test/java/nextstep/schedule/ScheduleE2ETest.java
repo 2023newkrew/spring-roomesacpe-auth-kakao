7ec9e8e202b2e9667671b05499391d47cb89143f
kokomo0 <50624525+kokomo0@users.noreply.github.com>
2023-01-25 20:44:35 +0900

    test: 관리자/유저 권한의 멤버가 스케줄을 생성/삭제 하는 테스트코드 추가

--- src/test/java/nextstep/schedule/ScheduleE2ETest.java
+++ src/test/java/nextstep/schedule/ScheduleE2ETest.java
@@ -1,6 +1,10 @@
 package nextstep.schedule;
 
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import nextstep.auth.TokenRequest;
+import nextstep.auth.TokenResponse;
+import nextstep.member.MemberRequest;
 import nextstep.theme.ThemeRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -17,9 +21,44 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ScheduleE2ETest {
 
     private Long themeId;
+    private String adminToken;
+    private String userToken;
 
     @BeforeEach
     void setUp() {
+        //어드민 멤버, 유저 멤버 생성
+        RestAssured
+                .given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(new MemberRequest("kayla", "password1", "kbpark", "010-1234-5678", "admin"))
+                .when().post("/members")
+                .then();
+
+        RestAssured
+                .given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(new MemberRequest("userA", "qwer12345", "june", "010-1234-5678", "user"))
+                .when().post("/members")
+                .then();
+
+        //어드민 멤버, 유저 멤버 로그인
+        adminToken = RestAssured
+                .given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(new TokenRequest("kayla", "password1"))
+                .when().post("/login/token")
+                .then()
+                .extract().as(TokenResponse.class).getAccessToken();
+
+        userToken = RestAssured
+                .given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(new TokenRequest("userA", "qwer12345"))
+                .when().post("/login/token")
+                .then()
+                .extract().as(TokenResponse.class).getAccessToken();
+
+        //테마 생성
         ThemeRequest themeRequest = new ThemeRequest("테마이름", "테마설명", 22000);
         var response = RestAssured
                 .given().log().all()
@@ -36,20 +75,21 @@ public class ScheduleE2ETest {
     @DisplayName("스케줄을 생성한다")
     @Test
     public void createSchedule() {
-        ScheduleRequest body = new ScheduleRequest(themeId, "2022-08-11", "13:00");
-        RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(body)
-                .when().post("/schedules")
-                .then().log().all()
-                .statusCode(HttpStatus.CREATED.value());
+        ExtractableResponse response = requestCreateSchedule(adminToken);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    @DisplayName("관리자가 아닌 멤버는 스케줄을 생성할 수 없다")
+    @Test
+    public void rejectCreateSchedule() {
+        ExtractableResponse response = requestCreateSchedule(userToken);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
     }
 
     @DisplayName("스케줄을 조회한다")
     @Test
     public void showSchedules() {
-        requestCreateSchedule();
+        requestCreateSchedule(adminToken);
 
         var response = RestAssured
                 .given().log().all()
@@ -63,30 +103,40 @@ public class ScheduleE2ETest {
         assertThat(response.jsonPath().getList(".").size()).isEqualTo(1);
     }
 
-    @DisplayName("예약을 삭제한다")
+    @DisplayName("관리자가 아닌 멤버는 스케줄을 삭제할 수 없다")
     @Test
     void delete() {
-        String location = requestCreateSchedule();
+        String location = requestCreateSchedule(adminToken).header("Location");
+        ExtractableResponse response = requestDeleteSchedule(userToken, "/admin" + location);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+    }
 
-        var response = RestAssured
-                .given().log().all()
-                .when().delete(location)
-                .then().log().all()
-                .extract();
-
+    @DisplayName("스케줄을 삭제한다")
+    @Test
+    void rejectDelete() {
+        String location = requestCreateSchedule(adminToken).header("Location");
+        ExtractableResponse response = requestDeleteSchedule(adminToken, "/admin" + location);
         assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 
-    public static String requestCreateSchedule() {
+    private ExtractableResponse requestCreateSchedule(String token) {
         ScheduleRequest body = new ScheduleRequest(1L, "2022-08-11", "13:00");
         return RestAssured
-                .given().log().all()
+                .given()
+                .auth().oauth2(token)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(body)
-                .when().post("/schedules")
-                .then().log().all()
-                .statusCode(HttpStatus.CREATED.value())
-                .extract()
-                .header("Location");
+                .when().post("/admin/schedules")
+                .then()
+                .extract();
+    }
+
+    private ExtractableResponse requestDeleteSchedule(String token, String location) {
+        return RestAssured
+                .given()
+                .auth().oauth2(token)
+                .when().delete(location)
+                .then()
+                .extract();
     }
 }
