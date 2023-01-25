3e5a0616cf0049e6c29e004593e0727f1436afdd
wilgur513 <wilgur513@naver.com>
2023-01-25 15:31:37 +0900

    refactor: 에러 로그 기록 추가

--- src/main/java/nextstep/auth/AuthAdvice.java
+++ src/main/java/nextstep/auth/AuthAdvice.java
@@ -2,6 +2,8 @@ package nextstep.auth;
 
 import nextstep.support.InvalidLoginException;
 import nextstep.support.UnauthorizedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,8 +11,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class AuthAdvice {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthAdvice.class);
+
     @ExceptionHandler({InvalidLoginException.class, UnauthorizedException.class})
     public ResponseEntity<String> onException(Exception e) {
+        LOGGER.debug(e.getMessage());
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 }
