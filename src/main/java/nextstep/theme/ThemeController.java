a357b523941b842f13e1c6d344528772d20d62a1
kokomo0 <50624525+kokomo0@users.noreply.github.com>
2023-01-25 23:18:47 +0900

    feat: 관리자/유저 권한의 멤버가 테마를 생성/삭제 하는 테스트코드 추가

--- src/main/java/nextstep/theme/ThemeController.java
+++ src/main/java/nextstep/theme/ThemeController.java
@@ -1,19 +1,19 @@
 package nextstep.theme;
 
+import nextstep.auth.AccessType;
+import nextstep.member.MemberRole;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/themes")
 public class ThemeController {
     private ThemeService themeService;
 
@@ -21,19 +21,21 @@ public class ThemeController {
         this.themeService = themeService;
     }
 
-    @PostMapping
+    @PostMapping("/admin/themes")
+    @AccessType(role = MemberRole.ADMIN)
     public ResponseEntity<Void> createTheme(@RequestBody ThemeRequest themeRequest) {
         Long id = themeService.create(themeRequest);
         return ResponseEntity.created(URI.create("/themes/" + id)).build();
     }
 
-    @GetMapping
+    @GetMapping("/themes")
     public ResponseEntity<List<Theme>> showThemes() {
         List<Theme> results = themeService.findAll();
         return ResponseEntity.ok().body(results);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/admin/themes/{id}")
+    @AccessType(role = MemberRole.ADMIN)
     public ResponseEntity<Void> deleteTheme(@PathVariable Long id) {
         themeService.delete(id);
 
