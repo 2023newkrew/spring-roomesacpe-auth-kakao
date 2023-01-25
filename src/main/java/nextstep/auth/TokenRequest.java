f9229ca140c0fde5332eea2bd9a2952d22c85853
gkwodn1044 <gkwodn1044@gmail.com>
2023-01-25 19:34:59 +0900

    feat : Request DTO에서 기본 생성자 제거

--- src/main/java/nextstep/auth/TokenRequest.java
+++ src/main/java/nextstep/auth/TokenRequest.java
@@ -4,9 +4,6 @@ public class TokenRequest {
     private String username;
     private String password;
 
-    public TokenRequest() {
-    }
-
     public TokenRequest(String username, String password) {
         this.username = username;
         this.password = password;
