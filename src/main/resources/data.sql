f8d6ae249d961b03faa240ae7ce2d1a50e42c7b0
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-25 20:02:44 +0900

    feat: Role 테이블과 Member 테이블을 초기화하는 insert문 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,6 @@
 INSERT INTO ADMIN (username, password, name, phone) values ('admin1', 'admin1', 'john', '010-1234-5678');
+
+INSERT INTO ROLE (name) values ('ADMIN');
+INSERT INTO ROLE (name) values ('USER');
+
+INSERT INTO MEMBER (username, password, name, phone, role_id) values ('admin2', 'admin2', 'kim', '010-8765-4321', 1);
