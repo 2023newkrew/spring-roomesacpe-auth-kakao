f0a18be35c389094f8b39789ab17811f8a055a06
jayden.v <jayden.v@kakaocorp.com>
2023-01-25 19:12:43 +0900

    chore: 초기 데이터 자동생성 구현

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO member (username, password, name, phone, role) VALUES ('admin', '123', 'admin', '010-1234-5678', 'ADMIN');
+INSERT INTO theme (id, name, desc, price) VALUES (1L, '테마이름', '테마설명', 20000);
