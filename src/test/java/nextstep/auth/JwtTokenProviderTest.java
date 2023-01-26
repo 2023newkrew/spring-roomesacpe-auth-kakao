cd831bbe17e548c89b23176eaee5ec9e3063a6ac
HONGMINJU <mhong0800@gmail.com>
2023-01-27 02:22:07 +0900

    test: 중복되는 테스트 삭제

--- src/test/java/nextstep/auth/JwtTokenProviderTest.java
+++ src/test/java/nextstep/auth/JwtTokenProviderTest.java
@@ -1,42 +1,34 @@
 package nextstep.auth;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import nextstep.auth.dto.TokenRequestDto;
 import nextstep.member.MemberRole;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
+@SpringBootTest
 @DisplayName("JwtTokenProvider 학습 테스트")
 class JwtTokenProviderTest {
 
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
+
     private static final TokenRequestDto tokenRequestDto = new TokenRequestDto("username",
         "password",
-        MemberRole.GENERAL.getName());
-
-    @Test
-    void createToken() {
-        JwtTokenProvider jwtTokenProvider = new JwtTokenProvider();
-
-        String token = jwtTokenProvider.createToken(tokenRequestDto);
-
-        assertThat(jwtTokenProvider.getUsername(token)).isNotNull();
-    }
+        MemberRole.GENERAL.toString());
 
     @Test
     void getPrincipal() {
-        JwtTokenProvider jwtTokenProvider = new JwtTokenProvider();
-
         String token = jwtTokenProvider.createToken(tokenRequestDto);
-
         assertThat(jwtTokenProvider.getUsername(token)).isEqualTo(tokenRequestDto.getUsername());
     }
 
-    // TODO 테스트 개선(Exception마다 상세하게)
     @Test
     void getPrincipalThrowExceptionTest() {
-        JwtTokenProvider jwtTokenProvider = new JwtTokenProvider();
         assertThatThrownBy(() -> jwtTokenProvider.getUsername("token"))
             .isInstanceOf(RuntimeException.class);
     }
