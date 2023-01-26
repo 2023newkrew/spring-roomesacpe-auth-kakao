737538c6776901dbf89e217b086bff1e0073679c
Nelfunction <oat641@gmail.com>
2023-01-26 15:42:17 +0900

    test: 테마 관련 테스트 수정
    - 생성/삭제 시 관리자 권한 검증 테스트

--- src/test/java/nextstep/theme/ThemeE2ETest.java
+++ src/test/java/nextstep/theme/ThemeE2ETest.java
@@ -1,6 +1,12 @@
 package nextstep.theme;
 
 import io.restassured.RestAssured;
+import nextstep.auth.TokenRequest;
+import nextstep.auth.TokenResponse;
+import nextstep.member.MemberRequest;
+import nextstep.reservation.ReservationRequest;
+import nextstep.schedule.ScheduleRequest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,17 +19,69 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class ThemeE2ETest {
-    @DisplayName("테마를 생성한다")
+    private Long memberId;
+    private String accessToken;
+    private String adminToken;
+
+    @BeforeEach
+    void setUp() {
+
+        // 멤버 추가
+        MemberRequest body = new MemberRequest("username", "password", "name", "010-1234-5678");
+        var memberResponse = RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(body)
+                .when().post("/members")
+                .then().log().all()
+                .statusCode(HttpStatus.CREATED.value())
+                .extract();
+        String[] memberLocation = memberResponse.header("Location").split("/");
+        memberId = Long.parseLong(memberLocation[memberLocation.length - 1]);
+
+        // 사용자 토큰 발급
+        TokenRequest tokenRequest = new TokenRequest("username", "password");
+        accessToken = RestAssured
+                .given().contentType(MediaType.APPLICATION_JSON_VALUE).body(tokenRequest)
+                .when().post("/login/token")
+                .then().statusCode(HttpStatus.OK.value()).extract().as(TokenResponse.class).getAccessToken();
+
+        // 관리자 토큰 발급
+        TokenRequest adminTokenRequest = new TokenRequest("admin", "password");
+        adminToken = RestAssured
+                .given().contentType(MediaType.APPLICATION_JSON_VALUE).body(adminTokenRequest)
+                .when().post("/login/token")
+                .then().statusCode(HttpStatus.OK.value()).extract().as(TokenResponse.class).getAccessToken();
+
+    }
+
+    @DisplayName("관리자로 테마를 생성한다")
     @Test
-    public void create() {
+    public void createByAdmin() {
         ThemeRequest body = new ThemeRequest("테마이름", "테마설명", 22000);
         RestAssured
                 .given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(body)
+                .auth().oauth2(adminToken)
                 .when().post("/themes")
                 .then().log().all()
                 .statusCode(HttpStatus.CREATED.value());
+    }
+
+    @DisplayName("관리자로 테마를 삭제한다")
+    @Test
+    void deleteByAdmin() {
+        Long id = createTheme();
+
+        var response = RestAssured
+                .given().log().all()
+                .auth().oauth2(adminToken)
+                .when().delete("/themes/" + id)
+                .then().log().all()
+                .extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 
     @DisplayName("테마 목록을 조회한다")
@@ -41,19 +99,36 @@ public class ThemeE2ETest {
         assertThat(response.jsonPath().getList(".").size()).isEqualTo(1);
     }
 
-    @DisplayName("테마를 삭제한다")
+
+    @DisplayName("일반 사용자로 테마 생성을 시도한다")
+    @Test
+    public void create() {
+        ThemeRequest body = new ThemeRequest("테마이름", "테마설명", 22000);
+        RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(body)
+                .auth().oauth2(accessToken)
+                .when().post("/themes")
+                .then().log().all()
+                .statusCode(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    @DisplayName("일반 사용자로 테마 삭제를 시도한다")
     @Test
     void delete() {
         Long id = createTheme();
 
         var response = RestAssured
                 .given().log().all()
+                .auth().oauth2(accessToken)
                 .when().delete("/themes/" + id)
                 .then().log().all()
                 .extract();
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
     }
+
 
     public Long createTheme() {
         ThemeRequest body = new ThemeRequest("테마이름", "테마설명", 22000);
@@ -61,6 +136,7 @@ public class ThemeE2ETest {
                 .given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(body)
+                .auth().oauth2(adminToken)
                 .when().post("/themes")
                 .then().log().all()
                 .statusCode(HttpStatus.CREATED.value())
