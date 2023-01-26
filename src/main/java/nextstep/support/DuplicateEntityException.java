a8145fb71d712c6b10ea18b6c165cacc303f220a
Nelfunction <oat641@gmail.com>
2023-01-26 14:43:07 +0900

    fix: 예외 메시지 추가
    - 예외 상황에 대한 설명을 메시지로 생성자에 전달

--- src/main/java/nextstep/support/DuplicateEntityException.java
+++ src/main/java/nextstep/support/DuplicateEntityException.java
@@ -5,4 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class DuplicateEntityException extends RuntimeException {
+    public DuplicateEntityException(String message) {
+        super(message);
+    }
 }
--- src/main/java/nextstep/support/NotExistEntityException.java
+++ src/main/java/nextstep/support/NotExistEntityException.java
@@ -5,4 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class NotExistEntityException extends RuntimeException {
+    public NotExistEntityException(String message) {
+        super(message);
+    }
 }
