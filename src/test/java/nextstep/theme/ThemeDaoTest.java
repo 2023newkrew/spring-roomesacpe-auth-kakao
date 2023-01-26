69614a16d5e3dba44050b6a8d0462389ad743e49
kokomo0 <50624525+kokomo0@users.noreply.github.com>
2023-01-26 15:43:25 +0900

    test: @Sql 어노테이션을 이용한 테마 삽입 테스트

--- src/test/java/nextstep/theme/ThemeDaoTest.java
+++ src/test/java/nextstep/theme/ThemeDaoTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.jdbc.Sql;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Sql(scripts = {"/test.sql"})
 @JdbcTest
 public class ThemeDaoTest {
     @Autowired
@@ -16,6 +18,6 @@ public class ThemeDaoTest {
     void save() {
         ThemeDao themeDao = new ThemeDao(jdbcTemplate);
         Long id = themeDao.save(new Theme("테마 이름", "테마 설명", 22_000));
-        assertThat(id).isNotNull();
+        assertThat(id).isEqualTo(4);
     }
 }
