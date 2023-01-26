6266a503fc89e03f3db68a730e36ff33378c5661
Nelfunction <oat641@gmail.com>
2023-01-26 17:10:37 +0900

    feat: 관리자 권한 적용
    - [x] 관리자 권한 적용
      - [x] 관리자 인증용 인터셉터 추가
      - [x] 테마 생성/수정/삭제 시 관리자 권한 검증

--- src/main/java/nextstep/auth/WebMvcConfiguration.java
+++ src/main/java/nextstep/auth/WebMvcConfiguration.java
@@ -2,6 +2,7 @@ package nextstep.auth;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -11,6 +12,12 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new AuthenticationPrincipalArgumentResolver());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/admin/**");
     }
 }
 
