00054ebbe06806b37999164099f37128a4124e7e
jungin0507 <a01033046758@gmail.com>
2023-01-27 14:18:30 +0900

    feat: Jwt 다루기 위한 클래스 구현

---
+++ src/main/java/nextstep/auth/jwt/DecodedJwtToken.java
@@ -0,0 +1,30 @@
+package nextstep.auth.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtHandlerAdapter;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+public class DecodedJwtToken {
+    private static final String secretKey = "learning-test-spring";
+    private final String rawToken;
+
+    public DecodedJwtToken(String rawToken) {
+        this.rawToken = rawToken;
+    }
+
+    public Long getPrincipal() {
+        return Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .build()
+                .parse(rawToken,
+                        new JwtHandlerAdapter<>() {
+                            @Override
+                            public Long onClaimsJws(Jws<Claims> jws) {
+                                return Long.parseLong(jws.getBody()
+                                        .getSubject());
+                            }
+                        });
+    }
+}
---
+++ src/main/java/nextstep/auth/jwt/EncodedJwtToken.java
@@ -0,0 +1,29 @@
+package nextstep.auth.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import java.util.Date;
+
+public class EncodedJwtToken {
+    private static final String secretKey = "learning-test-spring";
+    private static final Long validityInMilliseconds = 3600000L;
+
+    private final String principal;
+
+    public EncodedJwtToken(String principal) {
+        this.principal = principal;
+    }
+
+    public String getRawToken() {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setSubject(principal)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .compact();
+    }
+}
