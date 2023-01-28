33e71a657dbf54dee8d6e4b60b5fd5f2685fc059
knight7024 <knight7024@naver.com>
2023-01-29 01:41:19 +0900

    feat: 어드민 인터셉터 추가

--- src/main/java/nextstep/global/exception/NotAuthorizedException.java
+++ src/main/java/nextstep/global/exception/NotAuthorizedException.java
@@ -3,6 +3,6 @@ package nextstep.global.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
-public class UnauthenticatedException extends RuntimeException {
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class NotAuthorizedException extends RuntimeException {
 }
--- src/main/java/nextstep/theme/controller/ThemeController.java
+++ src/main/java/nextstep/theme/controller/ThemeController.java
@@ -13,7 +13,6 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/themes")
 public class ThemeController {
 
     private final ThemeService themeService;
@@ -23,21 +22,21 @@ public class ThemeController {
         this.themeService = themeService;
     }
 
-    @PostMapping
+    @PostMapping("/admin/themes")
     public ResponseEntity createTheme(@RequestBody ThemeRequest themeRequest) {
         Long id = themeService.create(themeRequest.getName(), themeRequest.getDesc(), themeRequest.getPrice());
 
         return ResponseEntity.created(URI.create("/themes/" + id)).build();
     }
 
-    @GetMapping
+    @GetMapping("/themes")
     public ResponseEntity<List<ThemeResponse>> showThemes() {
         List<Theme> themes = themeService.findAll();
 
         return ResponseEntity.ok().body(ThemeMapper.INSTANCE.domainListToResponseDtoList(themes));
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/admin/themes/{id}")
     public ResponseEntity deleteTheme(@PathVariable Long id) {
         themeService.deleteById(id);
 
