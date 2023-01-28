32447812f97db18f0cd7bf8363659ee57ffd7696
jungin0507 <a01033046758@gmail.com>
2023-01-28 19:25:48 +0900

    초기 유저 데이터 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO member (username, password, name, phone)
-VALUES ('username', 'password', 'name', '010-1234-5678');
+VALUES ('bayne', 'bayne', 'bayne', '010-1234-5678');
 INSERT INTO member (username, password, name, phone, role)
 VALUES ('admin', 'admin', 'admin', '010-1234-5678', 'ADMIN');
