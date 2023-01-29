9bbe680901369d0b38676d95f28226a0b49f9a3e
gh1224 <rlarkgus0@naver.com>
2023-01-29 13:27:43 +0900

    feat: member 테이블에 role 추가 + 토큰 클레임에 role 추가

--- src/main/java/nextstep/member/Member.java
+++ src/main/java/nextstep/member/Member.java
@@ -1,48 +1,25 @@
 package nextstep.member;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
     private Long id;
     private String username;
     private String password;
     private String name;
     private String phone;
-
-    public Member(Long id, String username, String password, String name, String phone) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.name = name;
-        this.phone = phone;
-    }
+    private String role;
 
     public Member(String username, String password, String name, String phone) {
         this.username = username;
         this.password = password;
         this.name = name;
         this.phone = phone;
-    }
-
-    public Member() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPhone() {
-        return phone;
     }
 
     public boolean checkWrongPassword(String password) {
