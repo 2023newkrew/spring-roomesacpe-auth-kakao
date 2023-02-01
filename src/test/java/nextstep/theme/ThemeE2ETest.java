e8d6cc531483eb6dfde1a52b7a547ab451a75720
HQkim <hyunkyu001@gmail.com>
2023-02-01 13:57:07 +0900

    test: 사용자가 테마를 생성,삭제하면 예외가 발생하는 테스트 추가

--- src/test/java/nextstep/theme/ThemeE2ETest.java
+++ src/test/java/nextstep/theme/ThemeE2ETest.java
@@ -2,8 +2,14 @@ package nextstep.theme;
 
 import io.restassured.RestAssured;
 import nextstep.CommonE2ETest;
+import nextstep.auth.TokenRequest;
+import nextstep.auth.TokenResponse;
+import nextstep.member.MemberRequest;
+import nextstep.member.Role;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -11,24 +17,62 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class ThemeE2ETest extends CommonE2ETest {
+    private static final String USERNAME = "username";
+    private static final String PASSWORD = "password";
+    private static final String NAME = "name";
 
-    @DisplayName("테마를 생성한다")
-    @Test
-    public void create() {
-        ThemeRequest body = new ThemeRequest("테마이름", "테마설명", 22000);
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+
+        MemberRequest body = new MemberRequest(USERNAME, PASSWORD, NAME, "010-1234-5678", Role.USER);
         RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(body)
+                .when().post("/members")
+                .then().log().all()
+                .statusCode(HttpStatus.CREATED.value())
+                .extract();
+    }
+
+    @DisplayName("관리자가 테마를 생성한다")
+    @Test
+    void create() {
+        ThemeRequest body = new ThemeRequest("테마이름", "테마설명", 22000);
+        var response = RestAssured
                 .given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .auth().oauth2(token.getAccessToken())
                 .body(body)
                 .when().post("/admin/themes")
                 .then().log().all()
-                .statusCode(HttpStatus.CREATED.value());
+                .extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    @DisplayName("사용자가 테마를 생성하면 예외가 발생한다")
+    @Test
+    void userCreate() {
+        String token = createBearerToken(USERNAME, PASSWORD);
+        ThemeRequest body = new ThemeRequest("테마이름", "테마설명", 22000);
+
+        var response = RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.AUTHORIZATION, token)
+                .body(body)
+                .when().post("/admin/themes")
+                .then().log().all()
+                .extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.FORBIDDEN.value());
     }
 
     @DisplayName("테마 목록을 조회한다")
     @Test
-    public void showThemes() {
+    void showThemes() {
         createTheme();
 
         var response = RestAssured
@@ -42,7 +86,7 @@ public class ThemeE2ETest extends CommonE2ETest {
         assertThat(response.jsonPath().getList(".").size()).isEqualTo(1);
     }
 
-    @DisplayName("테마를 삭제한다")
+    @DisplayName("관리자가 테마를 삭제한다")
     @Test
     void delete() {
         Long id = createTheme();
@@ -57,6 +101,24 @@ public class ThemeE2ETest extends CommonE2ETest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 
+    @DisplayName("사용자가 테마를 삭제하면 예외가 발생한다")
+    @Test
+    void userDelete() {
+        Long id = createTheme();
+        String token = createBearerToken(USERNAME, PASSWORD);
+
+        var response = RestAssured
+                .given().log().all()
+                .header(HttpHeaders.AUTHORIZATION, token)
+                .when().delete("/admin/themes/" + id)
+                .then().log().all()
+                .extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.FORBIDDEN.value());
+    }
+
+
+
     public Long createTheme() {
         ThemeRequest body = new ThemeRequest("테마이름", "테마설명", 22000);
         String location = RestAssured
@@ -70,5 +132,20 @@ public class ThemeE2ETest extends CommonE2ETest {
                 .extract().header("Location");
 
         return Long.parseLong(location.split("/")[2]);
+    }
+
+    private String createBearerToken(String username, String password) {
+        TokenRequest request = new TokenRequest(username, password);
+
+        TokenResponse response = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when().log().all()
+                .post("/login/token")
+                .then().log().all()
+                .extract()
+                .as(TokenResponse.class);
+
+        return "Bearer " + response.getAccessToken();
     }
 }
