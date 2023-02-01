45c7a171a7d9bf06a77908f5fcb7e0733a9a28aa
jungin0507 <a01033046758@gmail.com>
2023-02-01 12:34:24 +0900

    Auth Error는 401만 반환하도록 변경

--- src/main/java/nextstep/exception/AuthErrorCode.java
+++ src/main/java/nextstep/exception/AuthErrorCode.java
@@ -4,21 +4,18 @@ import org.springframework.http.HttpStatus;
 
 public enum AuthErrorCode implements ErrorCode {
 
-    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "아이디 혹은 비밀번호가 잘못됐습니다."),
-    TOKEN_VALIDATION_FAIL(HttpStatus.UNAUTHORIZED, "토큰 검증에 실패했습니다.");
-
-    private final HttpStatus httpStatus;
+    INVALID_PASSWORD("아이디 혹은 비밀번호가 잘못됐습니다."),
+    TOKEN_VALIDATION_FAIL("토큰 검증에 실패했습니다.");
 
     private final String message;
 
-    AuthErrorCode(HttpStatus httpStatus, String message) {
-        this.httpStatus = httpStatus;
+    AuthErrorCode(String message) {
         this.message = message;
     }
 
     @Override
     public HttpStatus getHttpStatus() {
-        return httpStatus;
+        return HttpStatus.UNAUTHORIZED;
     }
 
     @Override
