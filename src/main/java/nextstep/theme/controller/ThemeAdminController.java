595112b21588be852c8792c92482444e776beda5
BaeRoNuI <xogh20321@gmail.com>
2023-02-01 15:03:38 +0900

    reafactor: seperate admin request from ThemeController to ThemeAdminController

---
+++ src/main/java/nextstep/theme/controller/ThemeAdminController.java
@@ -0,0 +1,33 @@
+package nextstep.theme.controller;
+
+import nextstep.theme.dto.ThemeRequest;
+import nextstep.theme.entity.Theme;
+import nextstep.theme.service.ThemeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/themes")
+public class ThemeAdminController {
+    private ThemeService themeService;
+
+    public ThemeAdminController(ThemeService themeService) {
+        this.themeService = themeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createTheme(@RequestBody ThemeRequest themeRequest) {
+        Long id = themeService.create(themeRequest);
+        return ResponseEntity.created(URI.create("/themes/" + id)).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTheme(@PathVariable Long id) {
+        themeService.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/nextstep/theme/controller/ThemeController.java
+++ src/main/java/nextstep/theme/controller/ThemeController.java
@@ -16,7 +16,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/themes")
 public class ThemeController {
     private ThemeService themeService;
 
@@ -24,22 +24,9 @@ public class ThemeController {
         this.themeService = themeService;
     }
 
-    @PostMapping("/admin/themes")
-    public ResponseEntity<Void> createTheme(@RequestBody ThemeRequest themeRequest) {
-        Long id = themeService.create(themeRequest);
-        return ResponseEntity.created(URI.create("/themes/" + id)).build();
-    }
-
-    @GetMapping("/themes")
+    @GetMapping
     public ResponseEntity<List<Theme>> showThemes() {
         List<Theme> results = themeService.findAll();
         return ResponseEntity.ok().body(results);
-    }
-
-    @DeleteMapping("/admin/themes/{id}")
-    public ResponseEntity<Void> deleteTheme(@PathVariable Long id) {
-        themeService.delete(id);
-
-        return ResponseEntity.noContent().build();
     }
 }
