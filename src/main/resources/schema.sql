6d0e451018a7110b492f55281037ae6683fa7b8e
SJGeeKo <sungjun.gee.ko@gmail.com>
2023-02-02 14:23:00 +0900

    feat: 멤버 타입(일반, 관리자) 변경 기능 추가
    - MemberDao 내 editType 메서드 구현
    - MemberService 내 editType 메서드 구현
    - 비밀 키 저장을 위한 secrets.yml 작성
    - MemberController 내 changeUserType 메서드 구현
    - ChangeUserTypeRequest DTO 구현

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -31,5 +31,6 @@ CREATE TABLE member
     password varchar(20) not null,
     name     varchar(20) not null,
     phone    varchar(20) not null,
+    user_type varchar(20) not null,
     primary key (id)
 );
